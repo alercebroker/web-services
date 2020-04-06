f5dd3e2fd187118db7c9a45c2f152bdbc7281757
diego <diego@pop-os.localdomain>
2020-04-06 08:09:26 -0400

    added integration with APF added profiling

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM python:3.6
+
+
+ADD requirements.txt /app/
+WORKDIR /app
+RUN pip install --upgrade pip && pip install gunicorn==19.9.0
+RUN pip install numpy
+RUN pip install -r requirements.txt
+COPY /APF /APF
+RUN pip install -e /APF
+
+COPY . /app
+# WORKDIR /app/scripts
+EXPOSE 5000
+
+ENV ZTF_USER=""
+ENV ZTF_PASSWORD=""
+ENV ZTF_HOST=""
+ENV ZTF_PORT="5432"
+ENV ZTF_DATABASE=""
+ENV APP_WORKERS="2"
+ENV APP_BIND="0.0.0.0"
+ENV APP_PORT="5000"
+
+# CMD ["/bin/bash","entrypoint.sh"]
+ENV FLASK_APP="api.app"
+ENV FLASK_ENV="development"
+# CMD ["flask", "run", "--host=0.0.0.0"]
+WORKDIR /app/scripts
+CMD ["python", "run_profile.py"]
--- api/app.py
+++ api/app.py
@@ -3,15 +3,24 @@ from flask_cors import CORS
 from flask import Flask
 import logging
 from flask_caching import Cache
+from settings import DB_CONFIG, PROFILE_CONFIG
+import flask_profiler
 
 # Starting Flask API
 cache = Cache(config={'CACHE_TYPE': 'simple'})
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
+
+app.config["flask_profiler"] = PROFILE_CONFIG
+
+
 # Init cache
 cache.init_app(app)
 #Adding CORS for async calls
 CORS(app)
+# Get SQLAlchemy Session
+from apf.db.sql import get_session
+session = get_session(DB_CONFIG)
 
 #Check if gunicorn for logging
 is_gunicorn = "gunicorn" in os.environ.get("SERVER_SOFTWARE", "")
@@ -20,11 +29,15 @@ if is_gunicorn:
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
 
-#Default route
+# #Default route
 @app.route("/")
 def index():
-    return "Welcome to ALERCE PSQL API"
+    return "Welcome to ALERCE PSQL API test"
 
+from .internal import internal_blueprint
+app.register_blueprint(internal_blueprint)
 
 from .external import external_blueprint
 app.register_blueprint(external_blueprint)
+
+flask_profiler.init_app(app)
