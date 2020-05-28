d9de8bf5648a75c969847f2a6ad796c3576c0d09
Diego <diego@pop-os.localdomain>
2020-05-28 14:50:12 -0400

    using swagger doc

--- api/sql/resources/AstroObject.py
+++ api/sql/resources/AstroObject.py
@@ -1,9 +1,11 @@
 from flask_restful import fields, marshal_with, reqparse, Resource
 from flask import jsonify
+from flask_restful_swagger_3 import swagger
 from db_plugins.db.sql import query
 from db_plugins.db.sql.models import AstroObject
 from db_plugins.db.sql.serializers import AstroObjectSchema
-from api.app import session
+from .. import session
+
 parser = reqparse.RequestParser()
 parser.add_argument(['oid', 'object_id', 'id'], dest='oid')
 
@@ -11,9 +13,35 @@ parser.add_argument(['oid', 'object_id', 'id'], dest='oid')
 # Or maybe combine both
 fields = {}
 
+
 class ObjectResource(Resource):
-    def get(self,oid):
-        result = query(session, AstroObject,None, None,None,AstroObject.oid == oid)
+    """
+    Astro objects individual resource
+    """
+    @swagger.doc({
+        "summary":"Gets an individual object",
+        "description":"long description",
+        "parameters":[
+            {
+                "name": "oid",
+                "in": "path",
+                "description": "Identifier for the object",
+                "required": True,
+                "schema":{
+                    "type": "string"
+                }
+            }
+        ],
+        "responses":{
+            '200': {
+                'description': 'Ok',
+            }
+        }
+    }
+    )
+    def get(self, oid):
+        result = query(session, AstroObject, None, None,
+                       None, AstroObject.oid == oid)
         serializer = AstroObjectSchema()
         obj = result["results"][0]
         res = serializer.dump(obj)
@@ -21,6 +49,19 @@ class ObjectResource(Resource):
 
 
 class ObjectListResource(Resource):
+    """
+    Astro object list resource
+    """
+    @swagger.doc({
+        "summary":"Gets a list of objects",
+        "description":"long description",
+        "responses":{
+            '200': {
+                'description': 'Ok',
+            }
+        }
+    }
+    )
     def get(self):
         result = query(session, AstroObject, 1, 1)
         serializer = AstroObjectSchema()
--- api/sql/sql.py
+++ api/sql/sql.py
@@ -1,12 +1,11 @@
 from flask import Blueprint
-from flask_restful import Api
+from flask_restful_swagger_3 import Api
 from .resources.AstroObject import ObjectResource, ObjectListResource
 # import resources here
 
-
 sql_bp = Blueprint('sql', __name__)
+sql_api = Api(sql_bp, add_api_spec_resource=False )
 
-sql_api = Api(sql_bp)
 
 sql_api.add_resource(ObjectResource, "/astro_objects/<oid>")
 sql_api.add_resource(ObjectListResource, "/astro_objects")
