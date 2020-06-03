620197bc00f12206563c67f7f31107826ebac9f6
Diego <diego@pop-os.localdomain>
2020-06-03 09:38:29 -0400

    testing session initialization

--- api/db.py
+++ api/db.py
@@ -1,13 +1,16 @@
 from db_plugins.db.sql import get_scoped_session, start_db
-from flask import current_app 
-session = get_scoped_session(current_app.config["DATABASE"]["SQL"])
+from . import app
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+# session = get_scoped_session(app.config["DATABASE"]["SQL"])
+
+engine = create_engine(app.config["DATABASE"]["SQL"], convert_unicode=True)
+session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
+# Base = declarative_base()
+# Base.query = session.query_property()
 
 def init_db():
-    start_db(current_app.config["DATABASE"]["SQL"])
-
-def close_session(e=None):
-    if session is not None:
-        session.remove()
-
-def init_app(app):
-    app.teardown_appcontext(close_session)
+    from db_plugins.db.sql.models import Base, Class
+    Base.metadata.create_all(bind=engine)
+    # start_db(app.config["DATABASE"]["SQL"])
