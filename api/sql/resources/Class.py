cb0544fe49e4a47c2eb19159da3677b0c299d04a
Cristóbal Sepúlveda <cristobalsepulveda@MacBook-Air-de-Cristobal.local>
2020-06-05 11:42:12 -0400

    agrega documentacion

--- api/sql/resources/Class.py
+++ api/sql/resources/Class.py
@@ -2,6 +2,7 @@ from flask_restful import fields, marshal_with, reqparse, Resource
 from flask import jsonify
 from flask_restful import fields
 from flask_restful_swagger_3 import Schema, swagger
+
 from db_plugins.db.sql import query
 from db_plugins.db.sql.models import Class
 from db_plugins.db.sql.serializers import ClassSchema
@@ -10,20 +11,26 @@ from api.db import session
 parser = reqparse.RequestParser()
 parser.add_argument(['oid', 'object_id', 'id'], dest='oid')
 
-# Eventually replace serializer with fields and marshal_with
-# Or maybe combine both
 
 class ClassModel(Schema):
-    pass
+    type = 'object'
+    resource_fields = {
+        "name": fields.String,
+        "acronym": fields.String
+    }
+
+
+class ClassResponseModel(Schema):
+    type = 'array'
+    items = ClassModel
 
 
 class ClassResource(Resource):
     """
     Class individual resource
     """
-    """
     @swagger.doc({
-        "summary": "Gets an individual object",
+        "summary": "Gets an individual class",
         "description": "long description",
         "parameters": [
             {
@@ -40,13 +47,14 @@ class ClassResource(Resource):
             '200': {
                 'description': 'Class got',
                 'content': {
-                    "application/json": {}
+                    "application/json": {
+                        'schema': ClassModel
+                    }
                 }
             }
         }
     }
     )
-    """
     def get(self, name):
         result = query(session, Class, None, None, None, Class.name == name)
         serializer = ClassSchema()
@@ -59,21 +67,21 @@ class ClassListResource(Resource):
     """
     Class list resource
     """
-    """
     @swagger.doc({
         "summary": "Gets a list of classes",
         "description": "long description",
         "responses": {
             '200': {
-                'description': 'Class got',
+                'description': 'Classes got',
                 'content': {
-                    "application/json": {}
+                    "application/json": {
+                        'schema': ClassResponseModel
+                    }
                 }
             }
         }
     }
     )
-    """
     def get(self):
         result = query(session, Class, 1, 1)
         serializer = ClassSchema()
