b42cba0f136ae949ce8425fa72d55b9623bd1cc0
Cristóbal Sepúlveda <cristobalsepulveda@MacBook-Air-de-Cristobal.local>
2020-06-05 11:42:12 -0400

    agrega documentacion

--- api/sql/resources/Classifier.py
+++ api/sql/resources/Classifier.py
@@ -1,15 +1,28 @@
 from flask_restful import fields, marshal_with, reqparse, Resource
+from flask_restful_swagger_3 import Schema, swagger
 from flask import jsonify
+
 from db_plugins.db.sql import query
 from db_plugins.db.sql.models import Classifier
 from db_plugins.db.sql.serializers import ClassifierSchema
+from api.db import session
 
 parser = reqparse.RequestParser()
 parser.add_argument(['oid', 'object_id', 'id'], dest='oid')
 
-# Eventually replace serializer with fields and marshal_with
-# Or maybe combine both
-fields = {}
+
+class ClassifierModel(Schema):
+    type = 'object'
+    resource_fields = {
+        "name": fields.String,
+        "taxonomy_name": fields.String
+    }
+
+
+class ClassifierResponseModel(Schema):
+    type = 'array'
+    items = ClassifierModel
+
 
 class ClassifierResource(Resource):
     def get(self, name):
