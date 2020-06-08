e54e2b5f2fdf71627c827435b5d47adf41e23bd5
Diego <diego@pop-os.localdomain>
2020-06-08 14:24:01 -0400

    added parser documentation

--- api/sql/resources/AstroObject.py
+++ api/sql/resources/AstroObject.py
@@ -9,7 +9,7 @@ from api.db import session
 
 
 class ObjectModel(Schema):
-    type = 'object'
+    type = "object"
     resource_fields = {
         "oid": fields.String,
         "ndet": fields.Integer,
@@ -19,31 +19,27 @@ class ObjectModel(Schema):
         "dec": fields.Float,
         "xmatch_class_catalog": fields.String,
         "class_name": fields.String,
-        "probability": fields.Float
+        "probability": fields.Float,
     }
 
 
 class ResponseModel(Schema):
-    type = 'array'
+    type = "array"
     items = ObjectModel
 
 
 class ObjectResource(Resource):
-
-    @swagger.doc({
-        "summary": "Gets an individual object",
-        "description": "long description",
-        "responses": {
-            '200': {
-                'description': 'Ok',
-                'content': {
-                    'application/json': {
-                        'schema': ObjectModel
-                    }
+    @swagger.doc(
+        {
+            "summary": "Gets an individual object",
+            "description": "long description",
+            "responses": {
+                "200": {
+                    "description": "Ok",
+                    "content": {"application/json": {"schema": ObjectModel}},
                 }
-            }
+            },
         }
-    }
     )
     @marshal_with(ObjectModel.resource_fields)
     def get(self, oid):
@@ -53,62 +49,57 @@ class ObjectResource(Resource):
 
 class ObjectListResource(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('classifier', type=str, dest='classifier' )
-    parser.add_argument('class', type=str, dest='class' )
-    parser.add_argument('ndet_min', type=int, dest='ndet_min' )
-    parser.add_argument('probability', type=float, dest="probability")
-    @swagger.doc({
-        "summary": "Gets a list of objects",
-        "description": "long description",
-        "parameters":[
-            {
-                "name": "classifier",
-                "in": "path",
-                "description": "name of the classifier",
-                "required": False,
-                "schema": {"type": "string"}
-            },
-            {
-                "name": "class",
-                "in": "path",
-                "description": "name of the class",
-                "required": False,
-                "schema": {"type": "string"}
-            },
-            {
-                "name": "ndet_min",
-                "in": "path",
-                "description": "minimum number of detections",
-                "required": False,
-                "schema": {"type": "integer"}
-            },
-            {
-                "name": "probability",
-                "in": "path",
-                "description": "min class probability",
-                "required": False,
-                "schema": {"type": "integer"}
-            }
-        ],
-        "responses": {
-            '200': {
-                'description': 'Ok',
-                'content': {
-                    'application/json': {
-                        'schema': ResponseModel
-                    }
+    parser.add_argument(
+        "classifier",
+        type=str,
+        dest="classifier",
+        location="args",
+        help="classifier name",
+    )
+    parser.add_argument(
+        "class", type=str, dest="class", location="args", help="class name"
+    )
+    parser.add_argument(
+        "ndet_min",
+        type=int,
+        dest="ndet_min",
+        location="args",
+        help="minimum number of detections",
+    )
+    parser.add_argument(
+        "probability",
+        type=float,
+        dest="probability",
+        location="args",
+        help="minimum probability",
+    )
+
+    @swagger.doc(
+        {
+            "summary": "Gets a list of objects",
+            "description": "long description",
+            "reqparser": {"name": "object query parser", "parser": parser},
+            "responses": {
+                "200": {
+                    "description": "Ok",
+                    "content": {"application/json": {"schema": ResponseModel}},
                 }
-            }
+            },
         }
-    }
     )
     @marshal_with(ObjectModel.resource_fields)
     def get(self):
         args = self.parser.parse_args()
         params = self.parse_parameters(args)
         ret = []
-        for obj, clf in session.query(AstroObject, Classification).join(Classification).\
-                filter(*params).limit(10).offset(0).all():
+        for obj, clf in (
+            session.query(AstroObject, Classification)
+            .join(Classification)
+            .filter(*params)
+            .limit(10)
+            .offset(0)
+            .all()
+        ):
             ret.append({**obj.__dict__, **clf.__dict__})
         return ret
 
@@ -116,20 +107,18 @@ class ObjectListResource(Resource):
         classifier, class_ = True, True
         for arg in args:
             if args[arg] is not None:
-                if arg == 'classifier':
+                if arg == "classifier":
                     classifier = Classification.classifier_name == args[arg]
-                if arg == 'class':
+                if arg == "class":
                     class_ = Classification.class_name == args[arg]
         return classifier, class_
 
 
 class ObjectClassificationsResource(Resource):
-
     def get(self, oid):
         pass
 
 
 class ObjectXmatchResource(Resource):
-
     def get(self, oid):
         pass
