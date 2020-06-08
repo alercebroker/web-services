df70ca9945e57b71e66a82e3ada3017e52ed8f76
Diego <diego@pop-os.localdomain>
2020-06-08 14:23:24 -0400

    code formatting

--- api/__init__.py
+++ api/__init__.py
@@ -7,27 +7,36 @@ import flask_profiler
 from flask_restful_swagger_3 import get_swagger_blueprint
 from flask_swagger_ui import get_swaggerui_blueprint
 
+
 def get_credentials_from_config(config):
     db_credentials = None
     if "PSQL" in config:
         psql_config = config["PSQL"]
-        db_credentials = 'postgresql://{}:{}@{}:{}/{}'.format(
-            psql_config["USER"], psql_config["PASSWORD"], psql_config["HOST"], psql_config["PORT"], psql_config["DB_NAME"])
+        db_credentials = "postgresql://{}:{}@{}:{}/{}".format(
+            psql_config["USER"],
+            psql_config["PASSWORD"],
+            psql_config["HOST"],
+            psql_config["PORT"],
+            psql_config["DB_NAME"],
+        )
     elif "SQLITE" in config:
-        db_credentials = 'sqlite:///:memory:'
+        db_credentials = "sqlite:///:memory:"
     return db_credentials
 
 
 # Starting Flask API
 app = Flask(__name__, instance_relative_config=True)
-app.config['JSON_SORT_KEYS'] = False
+app.config["JSON_SORT_KEYS"] = False
 app.config["flask_profiler"] = PROFILE_CONFIG
 app.config["DATABASE"] = {"SQL": get_credentials_from_config(DB_CONFIG)}
 
 from .db import session, init_db
+
+
 @app.teardown_appcontext
 def cleanup(resp_or_exc):
-    session.remove()    
+    session.remove()
+
 
 # Adding CORS for async calls
 CORS(app)
@@ -36,7 +45,7 @@ CORS(app)
 # Check if gunicorn for logging
 is_gunicorn = "gunicorn" in os.environ.get("SERVER_SOFTWARE", "")
 if is_gunicorn:
-    gunicorn_logger = logging.getLogger('gunicorn.error')
+    gunicorn_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
 
@@ -52,13 +61,14 @@ from .sql.sql import sql_api
 docs.append(sql_api.get_swagger_doc())
 app.register_blueprint(sql_api.blueprint)
 app.register_blueprint(get_swagger_blueprint(docs))
-app.register_blueprint(get_swaggerui_blueprint(
-    '/docs',
-    'http://localhost:8085/api/swagger.json',
-    config={
-        "app_name": "ZTF API"
-    }
-), url_prefix='/docs')
+app.register_blueprint(
+    get_swaggerui_blueprint(
+        "/docs",
+        "http://localhost:8085/api/swagger.json",
+        config={"app_name": "ZTF API"},
+    ),
+    url_prefix="/docs",
+)
 
 
 flask_profiler.init_app(app)
