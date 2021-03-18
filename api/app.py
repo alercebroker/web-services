f31e81e8d78076ba0fc4c1e5723a5586a593581a
Camilo Valenzuela <cacke91@gmail.com>
2021-03-18 12:27:41 -0300

    Added host and prefix

--- api/app.py
+++ api/app.py
@@ -11,7 +11,7 @@ from flask_cors import CORS
 
 def create_app(config):
     app = Flask(__name__)
-    app.wsgi_app = ProxyFix(app.wsgi_app)
+    app.wsgi_app = ProxyFix(app.wsgi_app,x_host=1, x_prefix=1)
     app.config.from_object(config)
     CORS(app)
 
