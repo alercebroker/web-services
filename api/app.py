0f13c4bcb3b9297d617d06983ca623bf99e36265
Camilo Valenzuela <cacke91@gmail.com>
2021-03-26 10:04:06 -0300

    Changed description.md route

--- api/app.py
+++ api/app.py
@@ -22,7 +22,7 @@ def create_app(config):
             use_scoped=True,
         )
 
-        description = open("../description.md")
+        description = open("description.md")
 
         ztf_api = Api(
             title="ALeRCE API",
