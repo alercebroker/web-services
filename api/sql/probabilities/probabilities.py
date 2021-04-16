6884d986e10224f863fd1e0f505b74d7a22a6cba
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2021-04-16 11:22:17 -0400

    order probabilities

--- api/sql/probabilities/probabilities.py
+++ api/sql/probabilities/probabilities.py
@@ -5,9 +5,33 @@ from .models import probability_model
 from .parsers import prob_parser
 from werkzeug.exceptions import NotFound
 from ...db import db
+from typing import List
 
 api = Namespace("probabilities", description="Class probabilities related operations")
 api.models[probability_model.name] = probability_model
+
+LC_CLASSIFIER_ORDER = {
+    "SNIa": 0,
+    "SNIbc": 1,
+    "SNII": 2,
+    "SLSN": 3,
+    "QSO": 4,
+    "AGN": 5,
+    "Blazar": 6,
+    "YSO": 7,
+    "CV/Nova": 8,
+    "LPV": 9,
+    "E": 10,
+    "DSCT": 11,
+    "RRL": 12,
+    "CEP": 13,
+    "Periodic-Other": 14,
+    "Periodic": 15,
+    "Transient": 16,
+    "Stochastic": 17,
+}
+
+STAMP_CLASSIFIER_ORDER = {"AGN": 0, "SN": 1, "VS": 2, "asteroid": 3, "bogus": 4}
 
 
 @api.route("/<id>/probabilities")
@@ -33,6 +57,16 @@ class Probabilities(Resource):
                 probs = probs.filter(
                     models.Probability.classifier_version == args["classifier_version"]
                 )
-            return probs.all()
+            return self.order_probs(probs.all())
         else:
             raise NotFound
+
+    def order_probs(self, probs: List):
+        def sorting_order(e):
+            if "lc_classifier" in e["classifier_name"]:
+                return LC_CLASSIFIER_ORDER[e["class_name"]]
+            elif "stamp_classifier" in e["classifier_name"]:
+                return STAMP_CLASSIFIER_ORDER[e["class_name"]]
+
+        probs.sort(key=sorting_order)
+        return probs
--- tests/test_probabilities.py
+++ tests/test_probabilities.py
@@ -1,4 +1,5 @@
 from fixtures import client
+from api.sql.probabilities.probabilities import Probabilities
 
 
 def test_get_probabilities(psql_service, client):
@@ -10,3 +11,285 @@ def test_get_probabilities(psql_service, client):
 def test_get_probabilities_not_found(psql_service, client):
     r = client.get("objects/ZTF2/probabilities")
     assert r.status_code == 404
+
+
+def test_order_probs():
+    probs = [
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "AGN",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "Blazar",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "CEP",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "CV/Nova",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "DSCT",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "E",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "LPV",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "Periodic-Other",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "QSO",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "RRL",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "SLSN",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "SNIa",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "SNIbc",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "SNII",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "YSO",
+        },
+        {
+            "classifier_name": "lc_classifier_periodic",
+            "class_name": "CEP",
+        },
+        {
+            "classifier_name": "lc_classifier_periodic",
+            "class_name": "DSCT",
+        },
+        {
+            "classifier_name": "lc_classifier_periodic",
+            "class_name": "E",
+        },
+        {
+            "classifier_name": "lc_classifier_periodic",
+            "class_name": "LPV",
+        },
+        {
+            "classifier_name": "lc_classifier_periodic",
+            "class_name": "Periodic-Other",
+        },
+        {
+            "classifier_name": "lc_classifier_periodic",
+            "class_name": "RRL",
+        },
+        {
+            "classifier_name": "lc_classifier_stochastic",
+            "class_name": "AGN",
+        },
+        {
+            "classifier_name": "lc_classifier_stochastic",
+            "class_name": "Blazar",
+        },
+        {
+            "classifier_name": "lc_classifier_stochastic",
+            "class_name": "CV/Nova",
+        },
+        {
+            "classifier_name": "lc_classifier_stochastic",
+            "class_name": "QSO",
+        },
+        {
+            "classifier_name": "lc_classifier_stochastic",
+            "class_name": "YSO",
+        },
+        {
+            "classifier_name": "lc_classifier_top",
+            "class_name": "Periodic",
+        },
+        {
+            "classifier_name": "lc_classifier_top",
+            "class_name": "Stochastic",
+        },
+        {
+            "classifier_name": "lc_classifier_top",
+            "class_name": "Transient",
+        },
+        {
+            "classifier_name": "stamp_classifier",
+            "class_name": "AGN",
+        },
+        {
+            "classifier_name": "stamp_classifier",
+            "class_name": "asteroid",
+        },
+        {
+            "classifier_name": "stamp_classifier",
+            "class_name": "bogus",
+        },
+        {
+            "classifier_name": "stamp_classifier",
+            "class_name": "SN",
+        },
+        {
+            "classifier_name": "stamp_classifier",
+            "class_name": "VS",
+        },
+    ]
+
+    probabilities_resource = Probabilities()
+    ordered = probabilities_resource.order_probs(probs)
+    assert ordered == [
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "SNIa",
+        },
+        {
+            "classifier_name": "stamp_classifier",
+            "class_name": "AGN",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "SNIbc",
+        },
+        {
+            "classifier_name": "stamp_classifier",
+            "class_name": "SN",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "SNII",
+        },
+        {
+            "classifier_name": "stamp_classifier",
+            "class_name": "VS",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "SLSN",
+        },
+        {
+            "classifier_name": "stamp_classifier",
+            "class_name": "asteroid",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "QSO",
+        },
+        {
+            "classifier_name": "lc_classifier_stochastic",
+            "class_name": "QSO",
+        },
+        {
+            "classifier_name": "stamp_classifier",
+            "class_name": "bogus",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "AGN",
+        },
+        {
+            "classifier_name": "lc_classifier_stochastic",
+            "class_name": "AGN",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "Blazar",
+        },
+        {
+            "classifier_name": "lc_classifier_stochastic",
+            "class_name": "Blazar",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "YSO",
+        },
+        {
+            "classifier_name": "lc_classifier_stochastic",
+            "class_name": "YSO",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "CV/Nova",
+        },
+        {
+            "classifier_name": "lc_classifier_stochastic",
+            "class_name": "CV/Nova",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "LPV",
+        },
+        {
+            "classifier_name": "lc_classifier_periodic",
+            "class_name": "LPV",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "E",
+        },
+        {
+            "classifier_name": "lc_classifier_periodic",
+            "class_name": "E",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "DSCT",
+        },
+        {
+            "classifier_name": "lc_classifier_periodic",
+            "class_name": "DSCT",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "RRL",
+        },
+        {
+            "classifier_name": "lc_classifier_periodic",
+            "class_name": "RRL",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "CEP",
+        },
+        {
+            "classifier_name": "lc_classifier_periodic",
+            "class_name": "CEP",
+        },
+        {
+            "classifier_name": "lc_classifier",
+            "class_name": "Periodic-Other",
+        },
+        {
+            "classifier_name": "lc_classifier_periodic",
+            "class_name": "Periodic-Other",
+        },
+        {
+            "classifier_name": "lc_classifier_top",
+            "class_name": "Periodic",
+        },
+        {
+            "classifier_name": "lc_classifier_top",
+            "class_name": "Transient",
+        },
+        {
+            "classifier_name": "lc_classifier_top",
+            "class_name": "Stochastic",
+        },
+    ]
