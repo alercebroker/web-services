009f8ba274035c92f3ac7dd043e19be2f2f2cede
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2022-03-15 17:00:04 -0300

    update gunicorn deployment

--- api/extensions.py
+++ api/extensions.py
@@ -3,7 +3,17 @@ from prometheus_flask_exporter import PrometheusMetrics
 import os
 
 is_gunicorn = "gunicorn" in os.environ.get("SERVER_SOFTWARE", "")
-if is_gunicorn:
-    prometheus_metrics = GunicornInternalPrometheusMetrics.for_app_factory()
+environment = os.environ.get("ENVIRONMENT", "develop")
+
+if environment == "production":
+    if is_gunicorn:
+        prometheus_metrics = GunicornInternalPrometheusMetrics.for_app_factory()
+    else:
+        prometheus_metrics = PrometheusMetrics.for_app_factory()
 else:
-    prometheus_metrics = PrometheusMetrics.for_app_factory()
+
+    class Mock:
+        def init_app(self, app):
+            return None
+
+    prometheus_metrics = Mock()
