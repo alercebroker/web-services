5121c18a91888a81e64e4fd5daef538578790b5c
Alex Alvarez Toledo <otajla@hotmail.com>
2022-04-12 10:57:37 -0400

    feature: tests for ralidator core

--- tests/test_ralidator_core/test_ralidator_core.py
+++ tests/test_ralidator_core/test_ralidator_core.py
@@ -96,7 +96,30 @@ def test_check_allowed_allowed():
     assert result
 
 def test_check_allowed_not_allowed():
-    pass
+    test_callbacks_dict = {
+        "filter1": lambda x: x,
+        "filter2": lambda x: x,
+        "filter3": lambda x: x
+    }
+    token = {
+        'token_type': 'access',
+        'exp': datetime.now(tz=timezone.utc) + timedelta(hours=1),
+        'jti': 'test_jti',
+        'user_id': 1,
+        "permisions": ["permision1", "permision2"],
+        "filters": ["filter1", "filter2"    ]
+    }
+    encripted_token = jwt.encode(token, TEST_SECRET_KEY, algorithm="HS256")
+
+    ralidator = Ralidator(test_callbacks_dict)
+    ralidator.authenticate_token(TEST_SECRET_KEY, encripted_token)
+    ralidator.set_required_permisions(["permision3"])
+    result = ralidator.check_if_allowed()
+    assert not result
 
 def test_apply_filters():
+    # pendiente, una idea es aplicar todos los filtros a un
+    # elemnto de la lista, si se queda al final se agrega al
+    # resultado si es none tras algun filtro, se pasa al siguiente
+
     pass
