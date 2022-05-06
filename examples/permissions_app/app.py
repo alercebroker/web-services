1be1084134e0b15fd0621fdbef257bb8d36996b8
Alex Alvarez Toledo <otajla@hotmail.com>
2022-05-06 10:04:30 -0400

    testing: now the settigins names are uppercase

--- examples/permissions_app/app.py
+++ examples/permissions_app/app.py
@@ -10,9 +10,9 @@ def create_app() -> Flask:
     app = Flask(__name__)
     app.config["FILTERS_MAP"] = {}
     app.config["RALIDATOR_SETTINGS"] = {
-        "user_api_url": "user_api_url",
-        "user_api_token": "user_api_token",
-        "secret_key": TEST_SECRET_KEY,
+        "USER_API_URL": "user_api_url",
+        "USER_API_TOKEN": "user_api_token",
+        "SECRET_KEY": TEST_SECRET_KEY,
     }
     ralidator.init_app(app)
     app.register_blueprint(main_views)
