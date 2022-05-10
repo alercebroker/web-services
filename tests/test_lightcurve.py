947def998ba2eb59ac9d4cc673a9b522137d8467
Alex Alvarez Toledo <otajla@hotmail.com>
2022-05-10 18:54:38 -0400

    testing: changed reference in ralidator flask install. Fixed lightcurve test

--- tests/test_lightcurve.py
+++ tests/test_lightcurve.py
@@ -37,7 +37,7 @@ def test_get_lightcurve_not_found(client):
 def test_get_lightcurve_forbidden(client, app):
     test_secret_key = app.config["RALIDATOR_SETTINGS"]["SECRET_KEY"]
     token = create_token(["useles_permission"], [], test_secret_key)
-    headers = {"AUTH_TOKEN": token}
+    headers = {"AUTH-TOKEN": token}
     rv = client.get("objects/ZTF1/lightcurve?survey_id=ztf", headers=headers)
     assert rv.status_code == 403
 
@@ -47,7 +47,7 @@ def test_get_lightcurve_filters(client, app):
     token = create_token(
         ["basic_user"], ["filter_atlas_lightcurve"], test_secret_key
     )
-    headers = {"AUTH_TOKEN": token}
+    headers = {"AUTH-TOKEN": token}
     rv = client.get(
         "objects/ATLAS1/lightcurve?survey_id=atlas", headers=headers
     )
@@ -74,7 +74,7 @@ def test_get_detections_not_found(client):
 def test_get_detections_forbidden(client, app):
     test_secret_key = app.config["RALIDATOR_SETTINGS"]["SECRET_KEY"]
     token = create_token(["useles_permission"], [], test_secret_key)
-    headers = {"AUTH_TOKEN": token}
+    headers = {"AUTH-TOKEN": token}
     rv = client.get("objects/ZTF1/detections?survey_id=ztf", headers=headers)
     assert rv.status_code == 403
 
@@ -84,7 +84,7 @@ def test_get_detections_filters(client, app):
     token = create_token(
         ["basic_user"], ["filter_atlas_detections"], test_secret_key
     )
-    headers = {"AUTH_TOKEN": token}
+    headers = {"AUTH-TOKEN": token}
     rv = client.get(
         "objects/ATLAS1/detections?survey_id=atlas", headers=headers
     )
@@ -111,7 +111,7 @@ def test_get_non_detections_not_found(client):
 def test_get_non_detections_forbidden(client, app):
     test_secret_key = app.config["RALIDATOR_SETTINGS"]["SECRET_KEY"]
     token = create_token(["useles_permission"], [], test_secret_key)
-    headers = {"AUTH_TOKEN": token}
+    headers = {"AUTH-TOKEN": token}
     rv = client.get(
         "objects/ZTF1/non_detections?survey_id=ztf", headers=headers
     )
@@ -123,7 +123,7 @@ def test_get_non_detections_filters(client, app):
     token = create_token(
         ["basic_user"], ["filter_atlas_non_detections"], test_secret_key
     )
-    headers = {"AUTH_TOKEN": token}
+    headers = {"AUTH-TOKEN": token}
     rv = client.get(
         "objects/ATLAS1/non_detections?survey_id=atlas", headers=headers
     )
