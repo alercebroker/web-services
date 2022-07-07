5aef7fee6d376bb4eb5c98f9370af1907e39a455
Pablo Castellanos <pablocastellanos@gmail.com>
2022-07-07 15:08:52 -0400

    added magstats container

---
+++ src/core/magstats/container.py
@@ -0,0 +1,16 @@
+from dependency_injector import containers, providers
+from .infrastructure.magstats_repository import MongoMagStatsRepository
+from .domain.magstats_service import MagStatsService
+from .use_case.get_magstats import GetMagStats
+from db_plugins.db.mongo.connection import MongoConnection
+
+
+class MagStatsContainer(containers.DeclarativeContainer):
+    db = providers.Dependency(instance_of=MongoConnection)
+    repository = providers.Factory(MongoMagStatsRepository, db=db)
+
+    service = providers.Factory(
+        MagStatsService,
+        magstats_repository_factory=repository
+    )
+    command = providers.Factory(GetMagStats, service=service)
