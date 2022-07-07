b8da5e3d7ad51a400a670edec0dc3b65dc4bfabb
Pablo Castellanos <pablocastellanos@gmail.com>
2022-07-07 15:18:44 -0400

    Removed ZTF specific fields from magstats model

--- src/api/resources/magstats/models.py
+++ src/api/resources/magstats/models.py
@@ -1,20 +1,11 @@
-from flask_restx import Resource, fields, Model
+from flask_restx import fields, Model
 
 magstats_model = Model(
     "Magstats",
     {
         "fid": fields.Integer(description="Filter ID (1=g; 2=r, 3=i"),
-        "stellar": fields.Boolean(
-            description="whether the object appears to be unresolved in the given band"
-        ),
-        "corrected": fields.Boolean(
-            description="whether the corrected photometry should be used"
-        ),
         "ndet": fields.Integer(
             description="number of detections in the given band"
-        ),
-        "ndubious": fields.Integer(
-            description="number of dubious corrections"
         ),
         "magmean": fields.Float(
             description="the mean magnitude for the given fid"
@@ -43,8 +34,8 @@ magstats_model = Model(
         "lastmjd": fields.Float(
             description="the time of the last detection in the given fid"
         ),
-        "step_id_corr": fields.String(
-            description="correction step pipeline version"
+        "ingestion-step": fields.String(
+            description="ingestion step pipeline version"
         ),
     },
 )
