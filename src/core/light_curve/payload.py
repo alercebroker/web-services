331d70cbeaca51c855655a549a52fad239bc737a
Pablo Castellanos <pablocastellanos@gmail.com>
2022-07-18 14:08:58 -0400

    Updated output models

--- src/core/light_curve/payload.py
+++ src/core/light_curve/payload.py
@@ -5,7 +5,7 @@ class LightCurvePayload(MongoPayload):
     class LightCurveHelpers(MongoPayload.Helpers):
         @staticmethod
         def generate_tid_regex(string):
-            return f"{string.upper()}*"
+            return f"{string.upper()}.*"
 
     filter_rules = {
         "aid": MongoFilterRules(["aid"], None, str),
