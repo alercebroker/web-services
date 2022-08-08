e930c4425031cbe684c40d481680878d24635105
Pablo Castellanos <pablocastellanos@gmail.com>
2022-08-08 16:17:29 -0400

    Fixed post filter of probs repo

--- src/core/probabilities/infrastructure/probabilities_repository.py
+++ src/core/probabilities/infrastructure/probabilities_repository.py
@@ -5,9 +5,12 @@ class ProbabilitiesRepository(ObjectRepository):
     field = "probabilities"
 
     def _post_process(self, result, **kwargs):
-        if kwargs:
-            return [
-                elem
-                for elem in result
-                if all(elem[key] == value for key, value in kwargs.items())
-            ]
+        return [elem for elem in result if self.__check(elem, kwargs)]
+
+    @staticmethod
+    def __check(elem, filters):
+        return elem["classifier_name"] == (
+            filters["classifier"] or elem["classifier_name"]
+        ) and elem["classifier_version"] == (
+            filters["classifier_version"] or elem["classifier_version"]
+        )
