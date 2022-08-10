637eebf1ea9d7d1dce9686d3d0397e5e172cc53a
Pablo Castellanos <pablocastellanos@gmail.com>
2022-08-10 13:54:28 -0400

    Adapted FeaturesRepository for new ObjectRepo

--- src/core/features/infrastructure/features_repository.py
+++ src/core/features/infrastructure/features_repository.py
@@ -3,16 +3,3 @@ from shared.utils.repositories import ObjectRepository
 
 class FeaturesRepository(ObjectRepository):
     field = "features"
-
-    def _post_process(self, result, **kwargs):
-        return [elem for elem in result if self.__check(elem, kwargs)]
-
-    @staticmethod
-    def __check(elem, filters):
-        return elem["name"] == (
-            filters["name"] or elem["name"]
-        ) and elem["version"] == (
-            filters["version"] or elem["version"]
-        ) and elem["fid"] == (
-            filters["fid"] or elem["fid"]
-        )
