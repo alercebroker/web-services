089ae1367ac55853dbb463ad5a83f1bf67892162
Pablo Castellanos <pablocastellanos@gmail.com>
2022-08-10 12:57:20 -0400

    Added repository for features

--- src/core/features/domain/features_service.py
+++ src/core/features/domain/features_service.py
@@ -5,5 +5,5 @@ class FeaturesService:
     def __init__(self, repo_features):
         self.repo_features = repo_features
 
-    def get_probabilities(self, payload: FeaturesPayload):
+    def get_features(self, payload: FeaturesPayload):
         return self.repo_features.get(payload)
