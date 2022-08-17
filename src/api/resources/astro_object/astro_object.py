cb25c1e9ed4bbfeae86a6ac340d1fa0ba0147153
Pablo Castellanos <pablocastellanos@gmail.com>
2022-08-17 10:57:28 -0400

    Restored registration of object_item

--- src/api/resources/astro_object/astro_object.py
+++ src/api/resources/astro_object/astro_object.py
@@ -17,6 +17,7 @@ from . import models, parsers
 
 api = Namespace("objects", description="Objects related operations")
 api.models[models.object_list.name] = models.object_list
+api.models[models.object_item.name] = models.object_item
 api.models[models.single_object.name] = models.single_object
 api.models[models.limit_values.name] = models.limit_values
 
