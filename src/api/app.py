12e056526443ee749299ff404067763b1cb9891c
Pablo Castellanos <pablocastellanos@gmail.com>
2022-09-01 14:44:37 -0400

    Removed limiter

--- src/api/app.py
+++ src/api/app.py
@@ -1,10 +1,7 @@
 from werkzeug.middleware.proxy_fix import ProxyFix
 from flask import Flask
 from flask_restx import Api
-from api.resources.astro_object.astro_object import (
-    api as astro_object,
-    limiter,
-)
+from api.resources.astro_object.astro_object import api as astro_object
 from api.resources.light_curve.light_curve import api as light_curve
 from api.resources.magstats.magstats import api as magstats
 from api.resources.probabilities.probabilities import api as probabilities
@@ -69,7 +66,6 @@ def create_app(config_path):
         ztf_api.add_namespace(classifier, path="/classifiers")
         ztf_api.add_namespace(features, path="/objects")
         ztf_api.init_app(app)
-        limiter.init_app(app)
 
         def cleanup(e):
             db_control.cleanup_databases(e)
