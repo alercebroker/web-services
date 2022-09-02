057795e92882db014a04b3a2fccbd004c604e25a
Pablo Castellanos <pablocastellanos@gmail.com>
2022-09-02 17:25:36 -0400

    Refactored BDD tests for detections

--- examples/features/get_light_curve.feature
+++ examples/features/get_light_curve.feature
@@ -1,42 +1,18 @@
 Feature: Ask for a lightcurve data
+  Background: initialize database
 
-  Scenario: ask for detections for object from ztf survey data
-    Given the databases have ztf and atlas alerts
-    When request detections endpoint for object AID2 in ZTF survey
-    Then the request should return detections for the object from ztf data
-
-  Scenario: ask for non_detections for object from ztf survey data
-    Given the databases have ztf and atlas alerts
-    When request non_detections endpoint for object AID2 in ZTF survey
-    Then the request should return non detections for the object from ztf data
-
-  Scenario: ask for lightcurve for object from ztf survey data
-    Given the databases have ztf and atlas alerts
-    When request lightcurve endpoint for object AID2 in ZTF survey
-    Then the request should return detections and non detections for the object from ztf data
-
-  Scenario: ask for lightcurve for object from atlas survey data
-    Given the databases have ztf and atlas alerts
-    When request lightcurve endpoint for object AID2 in ATLAS survey
-    Then the request should return detections and non detections for the object from ATLAS data
-
-  Scenario: ask for detections for object from atlas survey data
-    Given the databases have ztf and atlas alerts
-    When request detections endpoint for object AID2 in ATLAS survey
-    Then the request should return detections for the object from atlas data
-
-  Scenario: ask for lightcurve for non stored object
-    Given the databases have ztf and atlas alerts
-    When request lightcurve endpoint for object ZTF999 in ZTF survey
-    Then the request should return 404 error
-
-  Scenario: ask for detections for non stored object
-    Given the databases have ztf and atlas alerts
-    When request detections endpoint for object ZTF999 in ZTF survey
-    Then the request should return 404 error
-
-  Scenario: ask for non detections for non stored object
-    Given the databases have ztf and atlas alerts
-    When request non_detections endpoint for object ZTF999 in ZTF survey
-    Then the request should return 404 error
-
+  Scenario Outline: ask for detections for object from ztf survey data
+    Given there is detection for object ALERCE1 with id ZTF1 from telescope ZTF
+    And there is detection for object ALERCE1 with id ZTF2 from telescope ZTF
+    And there is detection for object ALERCE1 with id ATLAS1 from telescope ATLAS1
+    And there is detection for object ALERCE1 with id ATLAS2 from telescope ATLAS2
+    When request detections for object ALERCE1 in <survey> survey <has permission>
+    Then retrieve detections with identifiers <oids>
+    Examples:
+      | has permission     | survey | oids                    |
+      | with permission    | ZTF    | ZTF1,ZTF2               |
+      | with permission    | ATLAS  | ATLAS1,ATLAS2           |
+      | with permission    | both   | ZTF1,ZTF2,ATLAS1,ATLAS2 |
+      | without permission | ZTF    | ZTF1,ZTF2               |
+      | without permission | ATLAS  | none                    |
+      | without permission | both   | ZTF1,ZTF2               |
--- examples/features/steps/get_light_curve.py
+++ examples/features/steps/get_light_curve.py
@@ -1,75 +1,33 @@
 import requests
 from behave import when, given, then
-from examples.features.environment import insert_mongo_data, build_admin_token
+from examples.features import environment
 
 
-BASE_URL = "http://alerts_api:5000/"
-HEADER_ADMIN_TOKEN = {
-    "AUTH-TOKEN": build_admin_token()
-}
+@given("there is {model} for object {aid} with id {oid} from telescope {tid}")
+def insert_all_detections(context, model, aid, oid, tid):
+    environment.insert_in_database(context, model, aid=aid, oid=oid, tid=tid)
 
 
-@given("the databases have ztf and atlas alerts")
-def database_setup_ztf_atlas(context):
-    insert_mongo_data(context)
+@when("request {endpoint} for object {aid} in {survey_id} survey {has} permission")
+def request_to_endpoint(context, endpoint, aid, survey_id, has):
+    req_string = f"{environment.BASE_URL}/objects/{aid}/{endpoint}"
+    if survey_id != "both":
+        req_string += f"?survey_id={survey_id.lower()}"
+
+    if has == "with":
+        context.result = requests.get(req_string, headers=environment.HEADER_ADMIN_TOKEN)
+    elif has == "without":
+        context.result = requests.get(req_string)
 
 
-@when("request {endpoint} endpoint for object {aid} in {survey_id} survey")
-def request_to_endpoint(context, endpoint, aid, survey_id):
-    res = requests.get(
-        f"{BASE_URL}/objects/{aid}/{endpoint}?survey_id={survey_id.lower()}",
-        headers=HEADER_ADMIN_TOKEN
-    )
-    if 200 <= res.status_code <= 400:
-        # Handle successful requests
-        context.result = res.json()
-    else:
-        context.result = res
-
-
-@then(
-    "the request should return detections and non detections for the object from {survey_id} data"
-)
-def check_lightcurve_response_for_survey(context, survey_id):
-    assert isinstance(context.result, dict)
-    if survey_id == "ZTF":
-        assert len(context.result["detections"]) == 1
-        assert context.result["detections"][0]["candid"] == "123"
-        assert len(context.result["non_detections"]) == 1
-        assert context.result["non_detections"][0]["mjd"] == 1
-        assert context.result["non_detections"][0]["fid"] == 1
-        assert context.result["non_detections"][0]["oid"] == "ZTF1"
-    if survey_id == "ATLAS":
-        assert len(context.result["detections"]) == 1
-        assert context.result["detections"][0]["candid"] == "candid"
-        assert len(context.result["non_detections"]) == 0
-
-
-@then(
-    "the request should return detections for the object from {survey_id} data"
-)
-def check_detections_response_for_survey(context, survey_id):
-    assert isinstance(context.result, list)
-    if survey_id == "ZTF":
-        assert len(context.result) == 1
-        assert context.result[0]["candid"] == "123"
-    if survey_id == "ATLAS":
-        assert len(context.result) == 1
-        assert context.result[0]["candid"] == "candid"
-
-
-@then(
-    "the request should return non detections for the object from {survey_id} data"
-)
-def check_non_detections_response_for_survey(context, survey_id):
-    assert isinstance(context.result, list)
-    if survey_id == "ZTF":
-        assert len(context.result) == 1
-        assert context.result[0]["mjd"] == 1
-        assert context.result[0]["fid"] == 1
-        assert context.result[0]["oid"] == "ZTF1"
-
-
-@then("the request should return {error_code} error")
-def check_error_response(context, error_code):
-    assert context.result.status_code == int(error_code)
+@then("retrieve detections with identifiers {oids}")
+def check_output_candid(context, oids):
+    assert context.result.status_code == 200
+    if oids == "none":  # Special case for empty return
+        assert len(context.result.json()) == 0
+        return
+    oids = set(oids.split(","))
+    for detection in context.result.json():
+        assert detection["oid"] in oids
+        oids.remove(detection["oid"])
+    assert len(oids) == 0
