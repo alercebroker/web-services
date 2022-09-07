942005b62d16a21cf571afa9fabaf07ffbb0f70c
Alex Alvarez Toledo <otajla@hotmail.com>
2022-09-07 11:31:40 -0300

    refactor: changing unset to project

--- src/core/astro_object/infrastructure/list_object_repository.py
+++ src/core/astro_object/infrastructure/list_object_repository.py
@@ -57,7 +57,7 @@ class ListAstroObjectRepository(MongoRepository):
                 }
             }
         )
-        pipe.append({"$unset": "probabilities"})
+        pipe.append({"$project" : { "probabilities": 0 } })
         if payload.sort:
             pipe.append({"$sort": payload.sort})
 
