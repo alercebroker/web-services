790031ecaf051488e2418422f9b7e670c54da665
Pablo Castellanos <pablocastellanos@gmail.com>
2022-10-27 10:26:49 -0300

    Added error counters

--- tests/performance/user_actions/index.js
+++ tests/performance/user_actions/index.js
@@ -1,10 +1,12 @@
 import http from "k6/http";
 import { check, fail, group, sleep } from "k6";
+import { Counter } from "k6/metrics";
 import { createUrl, getRandomElement } from "../common/index.js";
 
 import objects from "../common/objects.consts.js"
 
 const BASE_URL = "http://dev.api.alerce.online/alerts/v1/objects";
+var errors = new Counter("errors")
 
 export function queryObjectsWithClass(objectClass, pageSize, objectList) {
   const url = createUrl(BASE_URL, objectClass, pageSize);
@@ -16,6 +18,7 @@ export function queryObjectsWithClass(objectClass, pageSize, objectList) {
       "is query OK": (r) => r.status === 200,
     });
   });
+  errors.add(!isQuerySuccessful)
   sleep(2);
 }
 
@@ -29,10 +32,19 @@ export function retrieveObjectData(objectType) {
       ['GET', BASE_URL + '/' + oid + '/probabilities'],
     ]);
 
-    check(responses[0], {'lightcurve status is 200': (r) => r.status === 200});
-    check(responses[1], {'magstats status is 200': (r) => r.status === 200});
-    check(responses[2], {'probabilities status is 200': (r) => r.status === 200});
+    const isLightCurveSuccessful = check(responses[0], {
+      'lightcurve status is 200': (r) => r.status === 200,
+    });
+    const isMagStatsSuccessful = check(responses[1],
+      {'magstats status is 200': (r) => r.status === 200,
+    });
+    const isProbabilitiesSuccessful = check(responses[2],
+      {'probabilities status is 200': (r) => r.status === 200,
+    });
   });
+  errors.add(!isLightCurveSuccessful)
+  errors.add(!isProbabilitiesSuccessful)
+  errors.add(!isMagStatsSuccessful)
   sleep(2);
 }
 
@@ -44,9 +56,10 @@ export function retrieveDetectionsList(objectType) {
       const oid = objectPool[i]
       const detectionsResponse = http.get(BASE_URL + '/' + oid + '/detections');
 
-      check(detectionsResponse, {
+      const areDetectionsSuccessful = check(detectionsResponse, {
         "detections retrieved successfully": (r) => r.status === 200,
       });
+      errors.add(!areDetectionsSuccessful)
     }
   });
 }
