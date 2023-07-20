aba07b0bd390446ad87acae43b0278a2b51681cb
Diego Rodriguez <diegorodriguez@Diegos-MacBook-Pro.local>
2023-07-20 16:33:15 -0400

    feat: dynamic imports for prometheus

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,9 +1,12 @@
 from prometheus_flask_exporter.multiprocess import (
     GunicornInternalPrometheusMetrics,
 )
+import os
 
 
 def child_exit(server, worker):
-    GunicornInternalPrometheusMetrics.mark_process_dead_on_child_exit(
-        worker.pid
-    )
+    environment = os.environ.get("ENVIRONMENT", "develop")
+    if environment == "production":  # pragma: no cover
+        GunicornInternalPrometheusMetrics.mark_process_dead_on_child_exit(
+            worker.pid
+        )
