bb7d172835437ded6b16f28b6c9e9cc076a7c281
Pedro Gallardo <pedrogallardorobinson@gmail.com>
2023-07-25 17:03:17 -0400

    feat: added parse conesearch to sql repository implementation

--- astroobject/core/infrastructure/astroobject_sql_repository.py
+++ astroobject/core/infrastructure/astroobject_sql_repository.py
@@ -1,9 +1,11 @@
 from typing import List
 from astroobject.core.domain.astroobject_model import AstroObject
 from astroobject.core.domain.astroobject_queries import GetAstroObjectsQuery
+from sqlalchemy import text
 from sqlalchemy.orm import Session
 
 from ..domain.astroobject_repository import AstroObjectRepository
+
 
 class AstroObjectSQLRespository(AstroObjectRepository):
     def __init__(self, db_session: Session):
@@ -13,9 +15,17 @@ class AstroObjectSQLRespository(AstroObjectRepository):
         pass
 
     def _parse_conesearch(self, conesearch: dict):
-        pass
+        try:
+            ra, dec, radius = conesearch["ra"], conesearch["dec"], conesearch["radius"]
+        except KeyError:
+            ra, dec, radius = None, None, None
 
-    def _parse_filters(self, filters: dict): #?
+        if ra and dec and radius:
+            return text("q3c_radial_query(meanra, meandec,:ra, :dec, :radius)")
+        else:
+            return True
+
+    def _parse_filters(self, filters: dict):  # ?
         pass
 
     def get_objects(self, query: GetAstroObjectsQuery) -> List[AstroObject]:
