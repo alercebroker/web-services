4b8edba8a8386855499e02e0bdd4b73935f63311
mmolina2018 <mmolina2018@inf.udec.cl>
2023-09-04 17:01:21 -0300

    refactor: create structure and first try

---
+++ magstats/api/magstats.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, HTTPException, Path
+from sqlalchemy.orm import Session
+from api.container import AppContainer
+from db_plugins.db.sql import models
+from .models import MagstatsModel
+from dependency_injector.wiring import inject, Provide
+
+app = FastAPI()
+
+@app.get("/magstats/{id}", response_model= MagstatsModel)
+@inject
+def get_magstats(
+    id: int = Path(..., title="The object's identifier"),
+    session: Session = Provide[AppContainer.psql_db.provided.session],
+):
+    obj = session.query(models.Object).filter(models.Object.oid == id).one_or_none()
+    if obj:
+        return obj.magstats
+    else:
+        raise HTTPException(status_code=404, detail="Not found")
