d993828911764b94bc0b79e966b828edef9dd82a
Diego Rodriguez <diegorodriguez@Diegos-MacBook-Pro.local>
2023-09-05 11:40:58 -0300

    ci: fix for uptating more than one version

--- ci/build_staging.py
+++ ci/build_staging.py
@@ -2,13 +2,18 @@ import anyio
 from utils import build, get_tags, update_version, git_push
 
 
-def _build_package(package_dir: str, do_publish: bool, do_push: bool):
-    anyio.run(update_version, package_dir, "prerelease")
-    anyio.run(git_push, do_push)
+def _build_package(package_dir: str, do_publish: bool):
     tags = anyio.run(get_tags, package_dir)
     anyio.run(build, package_dir, tags, do_publish)
 
 
+def _update_version(packages: list, do_push: bool):
+    for package in packages:
+        anyio.run(update_version, package, "prerelease")
+    anyio.run(git_push, do_push)
+
+
 def build_staging(do_publish: bool, do_push: bool):
-    _build_package("lightcurve", do_publish, do_push)
-    _build_package("astroobject", do_publish, do_push)
+    _update_version(["lightcurve", "astroobject"], do_push)
+    _build_package("lightcurve", do_publish)
+    _build_package("astroobject", do_publish)
