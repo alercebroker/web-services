36cbab8b65b0767effe777be062d57446e1c89ec
Diego Rodriguez <diegorodriguez@Diegos-MacBook-Pro.local>
2023-09-06 15:47:06 -0300

    ci: add deploy pipeline

--- ci/main.py
+++ ci/main.py
@@ -1,14 +1,9 @@
 import sys
 from build_staging import build_staging
+from deploy_staging import deploy_staging
 
 
-def main():
-    stage = sys.argv[1]
-    dry_run = False
-    if len(sys.argv) > 2:
-        dry_run = sys.argv[2]
-        dry_run = dry_run == "--dry-run"
-        print("Running with --dry-run")
+def build(stage, dry_run):
     if stage == "staging":
         build_staging(dry_run)
     elif stage == "production":
@@ -19,5 +14,26 @@ def main():
         )
 
 
+def deploy(stage, dry_run):
+    if stage == "staging":
+        deploy_staging(dry_run)
+    elif stage == "production":
+        print("Production deploy not implemented yet")
+    else:
+        raise ValueError(
+            f'Invalid stage "{stage}". Valid stages are: staging, production'
+        )
+
+
 if __name__ == "__main__":
-    main()
+    command = sys.argv[1]
+    stage = sys.argv[2]
+    dry_run = False
+    if len(sys.argv) > 3:
+        dry_run = sys.argv[3]
+        dry_run = dry_run == "--dry-run"
+        print("Running with --dry-run")
+    if command == "build":
+        build(stage, dry_run)
+    if command == "deploy":
+        deploy(stage, dry_run)
