beb8bd5d974d2401a98e2cc55d3f5399049c3fe4
mmolina2018 <mmolina2018@inf.udec.cl>
2023-09-28 11:27:01 -0300

    cd: change path in dockerfile and add build

--- magstats/tests/test_service.py
+++ magstats/tests/test_service.py
@@ -2,7 +2,6 @@ from core.service import get_magstats
 from api.result_handler import handle_error
 from fastapi import HTTPException
 import pytest
-import unittest
 
 
 def test_get_ztf_magstats(psql_service, psql_session, init_psql):
@@ -28,16 +27,15 @@ def test_get_magstats_from_unknown_oid(psql_service, psql_session, init_psql):
     )
 
 
-class TestGetMagstatsSQL(unittest.TestCase):
-    def test_db_exception(self):
-        def mock_session_factory_with_exception():
-            raise Exception("Test exception")
 
-        with pytest.raises(HTTPException) as exc:
-            get_magstats(
-                session_factory=mock_session_factory_with_exception,
-                oid="oid1",
-                handle_error=handle_error,
-            )
+def mock_session_factory_with_exception():
+    raise Exception("Test exception")
 
-        assert exc.value.status_code == 500
+def test_get_magstats_with_db_exception():
+    with pytest.raises(HTTPException) as exc:
+        get_magstats(
+            session_factory=mock_session_factory_with_exception,
+            oid="oid1",
+            handle_error=handle_error,
+        )
+    assert exc.value.status_code == 500
