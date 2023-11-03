264004031b09272d60c2e308edd715105599967b
Edison Pizarro <edison.pizarro.godoy@gmail.com>
2023-11-03 05:03:11 -0300

    fix: healthcheck fix

--- ci/test/tests.py
+++ ci/test/tests.py
@@ -6,3 +6,13 @@ async def healthcheck_test(url: str):
         response = await client.get(url)
         assert response.status_code == 200
         assert response.text == "OK"
+
+tests_dict = {
+    "healthcheck": healthcheck_test,
+}
+
+if __name__ == "__main__":
+    import sys
+    test = sys.argv[1]
+    args = sys.argv[2].split(",")
+    tests_dict[test](*args)
--- ci/test/tests_stage_steps.py
+++ ci/test/tests_stage_steps.py
@@ -1,15 +1,21 @@
-import dagger
+import pathlib
 import sys
-from test.tests import healthcheck_test
+import dagger
 
 
 async def healthcheck_test_step(url: str):
     config = dagger.Config(log_output=sys.stdout)
     async with dagger.Connection(config) as client:
+        path = str(pathlib.Path().cwd().parent.absolute())
         container = (
-            client.container()
+            client.container()\
             .from_("python:3.11-slim")
+            .with_directory(
+                "/web-services",
+                client.host().directory(path, exclude=[".venv/", "**/.venv/"]),
+            )
+            .with_workdir("/web-services/ci/test")
             .with_exec(["pip", "install", "httpx"])
+            # Execute the healthcheck test inside the container
+            .with_exec(["python", "tests.py", "healthcheck", f"{url}"])
         )
-        # Execute the healthcheck test inside the container
-        container.run(healthcheck_test, url)
