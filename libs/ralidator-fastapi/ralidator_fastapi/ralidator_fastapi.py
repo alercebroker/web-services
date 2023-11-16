78189654edc677177e296688f554aff4c35919cc
Hector Larrañaga <HectorPLJ@gmail.com>
2023-11-16 15:11:04 -0300

    feat: add ignore_prefixes to ralidator

--- libs/ralidator-fastapi/ralidator_fastapi/ralidator_fastapi.py
+++ libs/ralidator-fastapi/ralidator_fastapi/ralidator_fastapi.py
@@ -1,19 +1,25 @@
+import re
+
 from ralidator_core.ralidator_core import (
     Ralidator,
     RalidatorCoreSettingsFactory,
 )
 from ralidator_core.settings_factory import json
-from starlette.requests import Request
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.responses import Response, StreamingResponse, JSONResponse
-from starlette.types import ASGIApp
 from starlette.concurrency import iterate_in_threadpool
-import re
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.responses import JSONResponse, Response, StreamingResponse
+from starlette.types import ASGIApp
 
 
 class RalidatorStarlette(BaseHTTPMiddleware):
     def __init__(
-        self, app: ASGIApp, config: dict, filters_map: dict, ignore_paths: list
+        self,
+        app: ASGIApp,
+        config: dict,
+        filters_map: dict,
+        ignore_paths: list,
+        ignore_prefixes: list,
     ) -> None:
         super().__init__(app)
         self.ralidator_settings = RalidatorCoreSettingsFactory.from_dict(
@@ -21,6 +27,7 @@ class RalidatorStarlette(BaseHTTPMiddleware):
         )
         self.filters_map = filters_map
         self.ignore_paths = ignore_paths
+        self.ignore_prefixes = ignore_prefixes
 
     async def response_to_json(self, response: StreamingResponse | Response):
         if isinstance(response, StreamingResponse):
@@ -58,13 +65,18 @@ class RalidatorStarlette(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next) -> Response:
         if request.url.path in self.ignore_paths:
             return await call_next(request)
+
+        for prefix in self.ignore_prefixes:
+            if request.url.path.startswith(prefix):
+                return await call_next(request)
+
         ralidator = Ralidator(self.ralidator_settings, self.filters_map)
         request.state.ralidator = ralidator
         self.apply_ralidator_auth(request, ralidator)
         response = await call_next(request)
-        
-        if response.status_code >= 400: #Error codes
+
+        if response.status_code >= 400:  # Error codes
             return response
-        
+
         response = await self.apply_ralidator_filters(response, ralidator)
         return response
