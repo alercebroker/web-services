34b5718106a5a880462c57399da616e805488561
ASHuenchuleo <arsepulh@gmail.com>
2023-11-20 15:42:21 -0300

    fix(lightcurve): Undid test code added to htmx.py

--- lightcurve/src/api/routes/htmx.py
+++ lightcurve/src/api/routes/htmx.py
@@ -1,6 +1,5 @@
 import os
 
-from data.load import get_dummy_features, get_dummy_lc
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
 from jinja2 import Environment, PackageLoader, select_autoescape
@@ -27,37 +26,32 @@ jinja_env.globals["API_URL"] = os.getenv("API_URL", "http://localhost:8000")
 
 @router.get("/lightcurve")
 async def lightcurve(oid: str) -> HTMLResponse:
-    #detections = get_detections(
-    #    oid=oid,
-    #    survey_id="ztf",
-    #    session_factory=session,
-    #    mongo_db=database,
-    #    handle_error=handle_error,
-    #    handle_success=handle_success,
-    #)
+    detections = get_detections(
+        oid=oid,
+        survey_id="ztf",
+        session_factory=session,
+        mongo_db=database,
+        handle_error=handle_error,
+        handle_success=handle_success,
+    )
 
-    #non_detections = get_non_detections(
-    #    oid=oid,
-    #    survey_id="ztf",
-    #    session_factory=session,
-    #    mongo_db=database,
-    #    handle_error=handle_error,
-    #    handle_success=handle_success,
-    #)
+    non_detections = get_non_detections(
+        oid=oid,
+        survey_id="ztf",
+        session_factory=session,
+        mongo_db=database,
+        handle_error=handle_error,
+        handle_success=handle_success,
+    )
 
-    #period = get_period(
-    #    oid=oid,
-    #    survey_id="ztf",
-    #    session_factory=session,
-    #    mongo_db=database,
-    #    handle_error=handle_error,
-    #    handle_success=handle_success,
-    #)
-    detections, non_detections = get_dummy_lc()
-    features = get_dummy_features()
-    period = list(
-        filter(lambda feat: feat.name == "Multiband_period", features)
-    )[0]
+    period = get_period(
+        oid=oid,
+        survey_id="ztf",
+        session_factory=session,
+        mongo_db=database,
+        handle_error=handle_error,
+        handle_success=handle_success,
+    )
 
     dr, dr_detections = await get_data_release(
         detections[0].ra, detections[0].dec
