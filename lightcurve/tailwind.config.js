9dce0f67bb12d1126204b4c8aa9c9626da5083f1
Hector Larrañaga <HectorPLJ@gmail.com>
2023-11-23 14:39:31 -0300

    fix: scope tailwind css preflight styles

--- lightcurve/tailwind.config.js
+++ lightcurve/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: ".tw-preflight",
+  prefix: 'tw-',
   darkMode: 'class',
   content: ["./src/api/templates/**/*.html.j2"],
   theme: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  }
 }
 
