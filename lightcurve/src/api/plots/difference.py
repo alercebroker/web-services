aed6d74554272afc9d0fb0f7f2c4fc73834d5770
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2024-04-03 11:50:28 -0300

    refactor(lightcurve): created shared module and fixed legend

--- lightcurve/src/api/plots/difference.py
+++ lightcurve/src/api/plots/difference.py
@@ -1,5 +1,6 @@
 from typing import List
 import logging
+from .shared import base_options, hex2rgb, get_bands
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -14,69 +15,6 @@ band_map = {
     5: {"name": "o", "color": "#FFA500"},
 }
 
-def base_options(text_color):
-    grid = {
-        "left": "7%",
-        "right": "5%",
-        "bottom": "20%",
-    }
-    tooltip = {
-        "show": True,
-        "trigger": "axis",
-        "axisPointer": {"type": "cross", "label": {"backgroundColor": "#505765"}},
-        # add formatter in js
-    }
-    toolbox = {
-        "show": True,
-        "showTitle": True,
-        "feature": {
-            "dataZoom": {
-                "show": True,
-                "title": {"zoom": "Zoom", "back": "Back"},
-                "icon": {
-                    "zoom": "M11,4A7,7 0 0,1 18,11C18,12.5 17.5,14 16.61,15.19L17.42,16H18L23,21L21,23L16,18V17.41L15.19,16.6C12.1,18.92 7.71,18.29 5.39,15.2C3.07,12.11 3.7,7.72 6.79,5.4C8,4.5 9.5,4 11,4M10,7V10H7V12H10V15H12V12H15V10H12V7H10M1,1V8L8,1H1Z",
-                    "back": "M21,11H6.83L10.41,7.41L9,6L3,12L9,18L10.41,16.58L6.83,13H21V11Z",
-                },
-            },
-            "restore": {"show": True, "title": "Restore"},
-        },
-        "tooltip": {
-            "bacgroundColor": "#222",
-            "textStyle": {"fontSize": 12},
-            "extraCssText": "box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);",
-        }
-    }
-    legend = {
-        "data": [],
-        "bottom": 0,
-        "textStyle": {"color": text_color, "fontWeight": "lighter"},
-    }
-    x_axis = {
-        "name": "Modified Julian Date",
-        "nameLocation": "center",
-        "scale": True,
-        "type": "value",
-        "splitLine": {"show": False},
-        "nameTextStyle": {"padding": 7}
-    }
-    y_axis = {
-        "name": "Magnitude",
-        "nameLocation": "start",
-        "type": "value",
-        "scale": True,
-        "splitLine": {"show": False},
-        "inverse": True,
-        ## add min and max in js
-    }
-    return {
-        "grid": grid,
-        "tooltip": tooltip,
-        "toolbox": toolbox,
-        "legend": legend,
-        "xAxis": x_axis,
-        "yAxis": y_axis,
-    
-    }
 
 def difference_lightcurve_options(
     detections, non_detections, forced_photometry, plot_text_color, ralidator
@@ -101,25 +39,9 @@ def difference_lightcurve_options(
     dict
         Difference lightcurve options for echarts.
     """
-    bands = get_bands(detections)
-    detection_series = get_detections_series(detections, bands)
-    detection_error_bars_series = get_error_bars_series(detections, bands)
-    non_detection_series = get_non_detections_series(non_detections, bands)
-    forced_photometry_series = get_forced_photometry_series(
-        forced_photometry, bands
-    )
-    forced_photometry_error_bars_series = get_error_bars_series(
-        forced_photometry, bands, forced=True
-    )
-    all_series = (
-        detection_series
-        + detection_error_bars_series
-        + non_detection_series
-        + forced_photometry_series
-        + forced_photometry_error_bars_series
-    )
     options = base_options(plot_text_color)
-    options["series"] = all_series
+    set_series(options, detections, non_detections, forced_photometry)
+    set_legend(options)
     return options
 
 
@@ -317,54 +239,57 @@ def get_forced_photometry_series(
 
     return list(map(lambda band: get_serie(band), bands))
 
-
-def get_bands(items: List[dict]) -> list:
-    """Get bands from item.
-
-    Parameters
-    ----------
-    item : list
-        List of items.
-        Each item has a fid field.
-
-    Returns
-    -------
-    list
-        Bands.
-
-    Raises
-    ------
-    KeyError
-        If the `fid` field is not present in the items.
-    """
-    try:
-        return list(set(map(lambda x: x["fid"], items)))
-    except KeyError:
-        raise KeyError(
-            "Error getting bands from items. `fid` field should be present in the items"
-        )
-
-
-def hex2rgb(hex: str, alpha: int = None) -> str:
-    """Parses hexadecimal color to rgba.
+def set_legend(options: dict):
+    """Set legend to options.
 
     Parameters
     ----------
-    hex : str
-    alpha : i
+    options : dict
+        Echarts options.
 
-    Returns
-    -------
-    str
-        the rgba representation
+    Note
+    ----
+    This function modifies the input options in place.
     """
-    r = int(hex[1:3], 16)
-    g = int(hex[3:5], 16)
-    b = int(hex[5:7], 16)
-    if alpha:
-        if alpha < 0:
-            alpha = 0
-        elif alpha > 1:
-            alpha = 1
-        return f"rgba({r}, {g}, {b}, {alpha})"
-    return f"rgb({r}, {g}, {b})"
+    data = []
+    for serie in options["series"]:
+        data.append(serie["name"])
+    options["legend"]["data"] = data
+
+
+def set_series(options: dict, detections, non_detections, forced_photometry) -> None:
+    """Set series to options.
+
+    Parameters
+    ----------
+    options : dict
+        Echarts options.
+    detections : list
+        List of detections.
+    non_detections : list
+        List of non detections.
+    forced_photometry : list
+        List of forced photometry.
+
+    Note
+    ----
+    This function modifies the input options in place.
+    """
+    bands = get_bands(detections)
+    detection_series = get_detections_series(detections, bands)
+    detection_error_bars_series = get_error_bars_series(detections, bands)
+    non_detection_series = get_non_detections_series(non_detections, bands)
+    forced_photometry_series = get_forced_photometry_series(
+        forced_photometry, bands
+    )
+    forced_photometry_error_bars_series = get_error_bars_series(
+        forced_photometry, bands, forced=True
+    )
+    all_series = (
+        detection_series
+        + detection_error_bars_series
+        + non_detection_series
+        + forced_photometry_series
+        + forced_photometry_error_bars_series
+    )
+    options["series"] = all_series
