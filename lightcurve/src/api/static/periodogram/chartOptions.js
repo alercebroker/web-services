08dc3f11ab9f815324b57debf68347db60c7f651
Hector Larrañaga <HectorPLJ@gmail.com>
2024-05-09 17:14:08 -0400

    feat(lc): move periodogram options and utils

--- lightcurve/src/api/static/periodogram/chartOptions.js
+++ lightcurve/src/api/static/periodogram/chartOptions.js
@@ -1,38 +1,42 @@
 export class PeriodogramOptions {
-  constructor(fontColor = 'fff') {
+  constructor(fontColor = "fff") {
     this.fontColor = fontColor;
 
     this.options = {
       grid: {
-        left: '0%',
-        right: '2%',
-        bottom: '10%',
+        left: "0%",
+        right: "2%",
+        bottom: "10%",
         containLabel: true,
       },
       title: {
-        text: 'Periodogram',
-        left: 'center',
+        text: "Periodogram",
+        left: "center",
         textStyle: {
-          fontWeight: 'lighter',
+          fontWeight: "lighter",
           color: this.fontColor,
         },
       },
       legend: {
-        type: "plain",
-        show: true
+        bottom: 0,
+        type: "scroll",
+        textStyle: {
+          fontWeight: "lighter",
+          color: this.fontColor,
+        },
       },
       tooltip: {
         show: true,
-        trigger: 'item',
+        trigger: "item",
         axisPointer: {
-          type: 'cross',
+          type: "cross",
           label: {
-            backgroundColor: '#505765',
+            backgroundColor: "#505765",
           },
         },
         formatter: (params) => {
           return `<b>Period:</b> ${params[0].data[0]} <b>Score:</b> ${params[0].data[1]}`;
-        }
+        },
       },
       toolbox: {
         show: true,
@@ -41,19 +45,17 @@ export class PeriodogramOptions {
           dataZoom: {
             show: true,
             title: {
-              zoom: 'Zoom',
-              back: 'Back',
+              zoom: "Zoom",
+              back: "Back",
             },
             icon: {
-              zoom:
-                'M11,4A7,7 0 0,1 18,11C18,12.5 17.5,14 16.61,15.19L17.42,16H18L23,21L21,23L16,18V17.41L15.19,16.6C12.1,18.92 7.71,18.29 5.39,15.2C3.07,12.11 3.7,7.72 6.79,5.4C8,4.5 9.5,4 11,4M10,7V10H7V12H10V15H12V12H15V10H12V7H10M1,1V8L8,1H1Z',
-              back:
-                'M21,11H6.83L10.41,7.41L9,6L3,12L9,18L10.41,16.58L6.83,13H21V11Z',
+              zoom: "M11,4A7,7 0 0,1 18,11C18,12.5 17.5,14 16.61,15.19L17.42,16H18L23,21L21,23L16,18V17.41L15.19,16.6C12.1,18.92 7.71,18.29 5.39,15.2C3.07,12.11 3.7,7.72 6.79,5.4C8,4.5 9.5,4 11,4M10,7V10H7V12H10V15H12V12H15V10H12V7H10M1,1V8L8,1H1Z",
+              back: "M21,11H6.83L10.41,7.41L9,6L3,12L9,18L10.41,16.58L6.83,13H21V11Z",
             },
           },
           restore: {
             show: true,
-            title: 'Restore',
+            title: "Restore",
           },
         },
       },
@@ -61,7 +63,7 @@ export class PeriodogramOptions {
         name: "Period",
         type: "value",
         type: "log",
-        nameLocation: 'center',
+        nameLocation: "center",
         scale: true,
         splitLine: {
           show: false,
@@ -72,7 +74,7 @@ export class PeriodogramOptions {
       yAxis: {
         name: "Score",
         type: "value",
-        nameLocation: 'end',
+        nameLocation: "end",
         scale: true,
         splitLine: {
           show: false,
@@ -80,7 +82,7 @@ export class PeriodogramOptions {
       },
       textStyle: {
         color: this.fontColor,
-        fontWeight: 'lighter',
+        fontWeight: "lighter",
       },
       animation: false,
       series: [
@@ -92,26 +94,26 @@ export class PeriodogramOptions {
           large: true,
           dimensions: [
             { name: "period", type: "float", displayName: "Period" },
-            { name: "score", type: "float", displayName: "Score" }
+            { name: "score", type: "float", displayName: "Score" },
           ],
           datasetIndex: 0,
           tooltip: {
             show: true,
-            trigger: 'item',
+            trigger: "item",
             axisPointer: {
-              type: 'cross',
+              type: "cross",
               label: {
-                backgroundColor: '#505765',
+                backgroundColor: "#505765",
               },
             },
             formatter: (params) => {
               return `<b>Period:</b> ${params.data[0]} <b>Score:</b> ${params.data[1]}`;
-            }
+            },
           },
         },
         {
           name: "Best periods",
-          type: "scatter", 
+          type: "scatter",
           symbol: "triangle",
           color: "red",
           symbolSize: 10,
@@ -119,51 +121,54 @@ export class PeriodogramOptions {
           large: true,
           dimensions: [
             { name: "period", type: "float", displayName: "Period" },
-            { name: "score", type: "float", displayName: "Score" }
+            { name: "score", type: "float", displayName: "Score" },
           ],
           datasetIndex: 1,
           tooltip: {
             show: true,
-            trigger: 'item',
+            trigger: "item",
             axisPointer: {
-              type: 'cross',
+              type: "cross",
               label: {
-                backgroundColor: '#505765',
+                backgroundColor: "#505765",
               },
             },
             formatter: (params) => {
               return `<b>Period:</b> ${params.data[0]} <b>Score:</b> ${params.data[1]}`;
-            }
+            },
           },
-        }
+        },
       ],
       dataset: [
         {
           dimensions: [
             { name: "period", type: "float", displayName: "Period" },
-            { name: "score", type: "float", displayName: "Score" }
+            { name: "score", type: "float", displayName: "Score" },
           ],
         },
         {
           dimensions: [
             { name: "period", type: "float", displayName: "Period" },
-            { name: "score", type: "float", displayName: "Score" }
+            { name: "score", type: "float", displayName: "Score" },
           ],
         },
-      ]
+      ],
     };
   }
 
   addSeries(periodogram) {
     const periods = {
       period: periodogram.period,
-      score: periodogram.score
-    }
+      score: periodogram.score,
+    };
     const best_periods = {
       period: periodogram.best_periods,
-      score: periodogram.best_periods_index.map((idx) => periodogram.score[idx])
-    }
+      score: periodogram.best_periods_index.map(
+        (idx) => periodogram.score[idx],
+      ),
+    };
     this.options.dataset[0].source = periods;
     this.options.dataset[1].source = best_periods;
   }
 }
+
