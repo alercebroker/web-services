ad7f8325cb216e03d27150db3ad758dc6b43cdf3
Claudio <c.mansillabrito@gmail.com>
2024-07-09 15:59:29 -0400

    update: darkmode working perfectly. added a function that detects tailwind changes and with that the radar chart can changes  his color

--- lightcurve/src/probability_api/routes/htmx.py
+++ lightcurve/src/probability_api/routes/htmx.py
@@ -1,13 +1,13 @@
 import re
 import os
 from typing import Annotated
-from fastapi import Query
+from fastapi import FastAPI, Request, Query
 import json
 
 from core.services.object import get_probabilities,get_taxonomies
 from fastapi import APIRouter, Request, HTTPException
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from ..result_handler import handle_error, handle_success
 
 router = APIRouter()
@@ -32,19 +32,21 @@ def prob_filter(prob_list, taxonomy_list):
     return taxonomy_dict, prob_dict
 
 @router.get("/probabilities/{oid}", response_class=HTMLResponse)
-async def object_crossmatch_app(
+async def object_probability_app(
     request: Request,
-    oid: str
+    oid: str,
 ):
+
     prob_list = get_probabilities(oid,session_factory = request.app.state.psql_session)
     taxonomy_list = get_taxonomies(session_factory = request.app.state.psql_session)
 
     taxonomy_dict, prob_dict = prob_filter(prob_list, taxonomy_list)
 
+
     return templates.TemplateResponse(
       name='probabilitiesCard.html.jinja',
       context={'request': request,
                'prob_dict': prob_dict,
-               'taxonomy_dict': taxonomy_dict
+               'taxonomy_dict': taxonomy_dict,
                },
   )
