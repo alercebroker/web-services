68093b200061faa2aceeebfb5870ea06a8d13416
Claudio <c.mansillabrito@gmail.com>
2024-07-29 17:18:02 -0400

    update: added functional slider and input. both are connected and both can add limits to the distance to hide or show the buttons

--- lightcurve/tailwind.config.js
+++ lightcurve/tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
             "./src/probability_api/templates//*.{html.jinja,html,css}",
             "./src/object_api/templates//*.{html.jinja,html,css}",
             "./src/magstats_api/templates//*.{html.jinja,html,css}",
-            "./src/scores_api/templates//*.{html.jinja,html,css}"
+            "./src/scores_api/templates//*.{html.jinja,html,css}",
+            "./src/crossmatch_api/templates//*.{html.jinja,html,css}",
            ],
   theme: {
     extend: {},
