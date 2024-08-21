bafe3f501ac89329d714502afb3f9105382b708b
Demurest <eric.apolonio@hotmail.com>
2024-08-21 15:02:40 -0400

    anomaly begin

---
+++ anomaly-api/anomaly_api/api.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.staticfiles import StaticFiles
+
+from .routes import rest
+
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(GZipMiddleware, minimum_size=1000)
+
+app.include_router(rest.router)
+
+app.mount("/static", StaticFiles(directory="./static"), name="static")
+
+
+@app.get("/openapi.json")
+def custom_swagger_route():
+    return app.openapi()
