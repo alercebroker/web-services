4c127b90bc3b42986cb1babd2d50db94d2782581
Claudio <c.mansillabrito@gmail.com>
2024-09-23 22:23:27 -0300

    added rest.py

---
+++ lightcurve/src/crossmatch_api/routes/rest.py
@@ -0,0 +1,30 @@
+import re
+import os
+from typing import Annotated
+from fastapi import Query
+import json
+
+#from core.services.object import get_mag_stats
+
+from core.services.object import get_object
+from fastapi import APIRouter, Request, HTTPException
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from ..result_handler import handle_error, handle_success
+from ..api_cross import get_alerce_data
+router = APIRouter()
+templates = Jinja2Templates(
+    directory="src/crossmatch_api/templates", autoescape=True, auto_reload=True
+)
+
+@router.get("/crossmatch/{oid}", response_class=HTMLResponse)
+async def object_mag_app(
+    request: Request,
+    oid: str,
+):
+    
+    object = get_object(oid,session_factory = request.app.state.psql_session)
+
+    cross = get_alerce_data(object.meanra, object.meandec, 50)
+  
+    return {'request': request, 'cross': cross}
