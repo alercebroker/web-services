e007f804cc81c6f36d30bcccaa9a23e4260efee4
Claudio Mansilla <139515383+ClaudioMansillaB@users.noreply.github.com>
2024-10-22 12:04:35 -0300

    Update get_crossmatch_data.py
    Deleted unnecesary if statement

--- lightcurve/src/crossmatch_api/get_crossmatch_data.py
+++ lightcurve/src/crossmatch_api/get_crossmatch_data.py
@@ -13,12 +13,6 @@ def get_alerce_data(ra, dec, radius):
     try:
         # Make the GET request
         response = requests.get(base_url, params=params)
-        
-        # Check if the request was successful
-        if response.status_code == 200:
-            return response.json()  # Return the JSON response
-        else:
-            return f"Error: Received status code {response.status_code}"
     
     except requests.RequestException as e:
         return f"Error: {str(e)}"
