4cfdcde677052908627908939d78ad2666ac401e
hectorxh <hectorplj@gmail.com>
2025-05-28 13:50:25 -0400

    fix: missing cronjob envars and fixed search route

--- tns_api/scripts/run_tns.py
+++ tns_api/scripts/run_tns.py
@@ -8,5 +8,6 @@ def run():
     root_path = os.getenv("ROOT_PATH", default="/")
     uvicorn.run("tns_api.api:app", host="0.0.0.0", port=int(port), root_path=root_path)
 
+
 if __name__ == "__main__":
     run()
