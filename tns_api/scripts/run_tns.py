a7836a47836e46f3928da412046e2b726d8c8a18
hectorxh <hectorplj@gmail.com>
2025-05-28 12:23:41 -0400

    fix: add root_path

--- tns_api/scripts/run_tns.py
+++ tns_api/scripts/run_tns.py
@@ -5,7 +5,8 @@ import uvicorn
 
 def run():
     port = os.getenv("PORT", default=8000)
-    uvicorn.run("tns_api.api:app", port=int(port), reload=True, reload_dirs=[".", "../libs"])
+    root_path = os.getenv("ROOT_PATH", default="/")
+    uvicorn.run("tns_api.api:app", host="0.0.0.0", port=int(port), root_path=root_path)
 
 if __name__ == "__main__":
     run()
