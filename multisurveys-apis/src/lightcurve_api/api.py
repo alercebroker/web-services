b55285f7e991f44622138f15f2743142581e79b5
Alex Alvarez <otajla@hotmail.com>
2025-07-04 15:26:12 -0400

    removed unnecesary import in lightcurve apo

--- multisurveys-apis/src/lightcurve_api/api.py
+++ multisurveys-apis/src/lightcurve_api/api.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.staticfiles import StaticFiles
 from prometheus_fastapi_instrumentator import Instrumentator
-from core.config.connection import psql_entity, connect
+from core.config.connection import psql_entity
 from .routes import rest
 
 app = FastAPI(openapi_url="/v2/lightcurve/openapi.json")
