bf0ac875ababc18848d7a15e40fa68851260bf50
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2025-08-29 11:27:00 -0400

    feat(conesearch): add coordinates-based search endpoint and add tests

--- multisurveys-apis/src/lightcurve_api/routes/conesearch.py
+++ multisurveys-apis/src/lightcurve_api/routes/conesearch.py
@@ -9,10 +9,10 @@ from core.idmapper.idmapper import catalog_oid_to_masterid
 from ..services.conesearch import conesearch as service
 from ..models.object import ApiObject
 
-router = APIRouter()
+router = APIRouter(prefix="/conesearch")
 
 
-@router.get("/conesearch_oid/objects")
+@router.get("/objects_by_oid")
 def conesearch(
     oid: str,
     survey: str,
@@ -22,11 +22,34 @@ def conesearch(
 ) -> List[ApiObject]:
     try:
         id = catalog_oid_to_masterid(survey, oid, True)
-    except ValueError:
-        raise HTTPException(status_code=400, detail="Invalid object ID")
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
     try:
-        return service.conesearch_oid(id, radius, neighbors, db.session)
+        result = service.conesearch_oid(id, radius, neighbors, db.session)
+        return result
+    except ValueError as ve:
+        raise HTTPException(status_code=400, detail=str(ve))
+    except Exception:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail="An error occurred")
+
+
+@router.get("/objects_by_coordinates")
+def conesearch_coordinates(
+    ra: float,
+    dec: float,
+    radius: float,
+    neighbors: int,
+    db: db_dependency,
+):
+    try:
+        result = service.conesearch_coordinates(
+            ra, dec, radius, neighbors, db.session
+        )
+        return result
+    except ValueError as ve:
+        raise HTTPException(status_code=400, detail=str(ve))
     except Exception:
         traceback.print_exc()
         raise HTTPException(status_code=500, detail="An error occurred")
