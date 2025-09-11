0d52f669fc6a9605da6fde27b93cab1e157faab9
Claudio Mansilla <c.mansillabrito@gmail.com>
2025-09-11 16:17:36 -0300

    changed router route to /htmx/mag. changed return to dummy_data

--- multisurveys-apis/src/magstat_api/routes/htmx.py
+++ multisurveys-apis/src/magstat_api/routes/htmx.py
@@ -5,7 +5,9 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from core.exceptions import ObjectNotFound
-from core.services.object import get_mag_stats
+from core.repository.queries.magstats import get_magstats_by_oid
+from core.repository.dummy_data import magstats_dummy
+
 
 router = APIRouter()
 templates = Jinja2Templates(
@@ -16,34 +18,36 @@ templates.env.globals["API_URL"] = os.getenv(
 )
 
 
-@router.get("/mag/{oid}", response_class=HTMLResponse)
+@router.get("/htmx/mag", response_class=HTMLResponse)
 async def object_mag_app(request: Request, oid: str):
-    try:
-        mag_stats = get_mag_stats(
-            oid, session_factory=request.app.state.psql_session
-        )
-    except ObjectNotFound:
-        raise HTTPException(status_code=404, detail="Object not found")
+    # try:
+    #     mag_stats = get_mag_stats(
+    #         oid, session_factory=request.app.state.psql_session
+    #     )
+    # except ObjectNotFound:
+    #     raise HTTPException(status_code=404, detail="Object not found")
 
-    mag_stats_dict = {}
-    for i, mag_stat in enumerate(mag_stats):
-        mag_stats_dict[f"band_{i+1}"] = {
-            "stellar": mag_stat.stellar,
-            "corrected": mag_stat.corrected,
-            "ndet": mag_stat.ndet,
-            "ndubious": mag_stat.ndubious,
-            "magmean": mag_stat.magmean,
-            "magmedian": mag_stat.magmedian,
-            "magmax": mag_stat.magmax,
-            "magmin": mag_stat.magmin,
-            "magsigma": mag_stat.magsigma,
-            "maglast": mag_stat.maglast,
-            "magfirst": mag_stat.magfirst,
-            "firstmjd": mag_stat.firstmjd,
-            "lastmjd": mag_stat.lastmjd,
-            "step_id_corr": mag_stat.step_id_corr,
-            "fid": mag_stat.fid,
-        }
+    # mag_stats_dict = {}
+    # for i, mag_stat in enumerate(mag_stats):
+    #     mag_stats_dict[f"band_{i+1}"] = {
+    #         "stellar": mag_stat.stellar,
+    #         "corrected": mag_stat.corrected,
+    #         "ndet": mag_stat.ndet,
+    #         "ndubious": mag_stat.ndubious,
+    #         "magmean": mag_stat.magmean,
+    #         "magmedian": mag_stat.magmedian,
+    #         "magmax": mag_stat.magmax,
+    #         "magmin": mag_stat.magmin,
+    #         "magsigma": mag_stat.magsigma,
+    #         "maglast": mag_stat.maglast,
+    #         "magfirst": mag_stat.magfirst,
+    #         "firstmjd": mag_stat.firstmjd,
+    #         "lastmjd": mag_stat.lastmjd,
+    #         "step_id_corr": mag_stat.step_id_corr,
+    #         "fid": mag_stat.fid,
+    #     }
+
+    mag_stats_dict = magstats_dummy
 
     return templates.TemplateResponse(
         name="magstatRebuild.html.jinja",
