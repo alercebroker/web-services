736cbcc99769a516067bd08061d969a96c4a9821
Claudio Mansilla <c.mansillabrito@gmail.com>
2025-09-11 16:16:45 -0300

    added magstats dummy data

--- multisurveys-apis/src/core/repository/dummy_data.py
+++ multisurveys-apis/src/core/repository/dummy_data.py
@@ -14,6 +14,45 @@ object_basic_information_dict = {
     "otherArchives": ['DESI Legacy Survey DR10', 'NED', 'PanSTARRS', 'SDSS DR18', 'SIMBAD', 'TNS', 'Vizier', 'VSX'],
 }
 
+magstats_dummy = [
+
+    {   
+        "fid": 1, 
+        "stellar": False, 
+        "corrected": False, 
+        "ndet": 38, 
+        "ndubious": 0, 
+        "magmean": 19.255001, 
+        "magmedian": 19.349136, 
+        "magmax": 20.2086, 
+        "magmin": 18.587002, 
+        "magsigma": 0.41979843, 
+        "maglast": 20.2086, 
+        "magfirst": 18.776695, 
+        "firstmjd": 59140.406, 
+        "lastmjd": 59260.14, 
+        "step_id_corr": "correction_0.0.1"
+    },
+
+    {   
+        "fid": 2, 
+        "stellar": False, 
+        "corrected": False, 
+        "ndet": 40, 
+        "ndubious": 0, 
+        "magmean": 18.685757, 
+        "magmedian": 18.596018, 
+        "magmax": 19.732458, 
+        "magmin": 18.37707, 
+        "magsigma": 0.28526792, 
+        "maglast": 19.732458, 
+        "magfirst": 18.89238, 
+        "firstmjd": 59140.258, 
+        "lastmjd": 59267.145, 
+        "step_id_corr": "correction_0.0.1"
+    }
+]
+
 tns_data_dict = {
     "object_data": {
         "discoverer": "-",
