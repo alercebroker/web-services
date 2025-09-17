ea850aa9cb5a74b09af1aba12719f2407db86390
Claudio Mansilla <c.mansillabrito@gmail.com>
2025-09-17 17:52:01 -0300

    added temporal router to make stamp card work

--- multisurvey-stamps/src/multisurvey_stamps/routes/rest.py
+++ multisurvey-stamps/src/multisurvey_stamps/routes/rest.py
@@ -1,7 +1,15 @@
-from fastapi import APIRouter, Request
-from fastapi import APIRouter, HTTPException, Request, Response
-from ...s3_handler import handler_selector
+from fastapi import APIRouter, Request, Response
+# from ...s3_handler import handler_selector
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from alerce.ms_stamps import AlerceStampsMultisurvey
+from alerce.search import AlerceSearch
+from astropy.time import Time
 
+router = APIRouter()
+templates = Jinja2Templates(
+    directory="src/multisurvey_stamps/templates", autoescape=True, auto_reload=True
+)
 
 router = APIRouter()
 
@@ -11,30 +19,89 @@ async def ping():
     return "This is the Stamps API"
 
 
-@router.get("/stamp")
-async def stamp(
-    request: Request,
+# @router.get("/stamp")
+# async def stamp(
+#     request: Request,
+#     oid: str,
+#     measurement_id: str,
+#     stamp_type: str,
+#     file_format: str,
+#     survey_id: str,
+# ):
+#     handler = handler_selector(survey_id)()
+
+#     _, file_buffer, mime = handler.get_stamp(
+#         oid, measurement_id, stamp_type, file_format
+#     )
+
+#     return Response(content=file_buffer.getvalue(), media_type=mime)
+
+
+# @router.get("/avro")
+# async def stamp(
+#     request: Request, oid: str, measurement_id: str, survey_id: str
+# ):
+#     handler = handler_selector(survey_id)()
+
+#     avro_json = handler.get_avro(oid, measurement_id)
+
+#     return avro_json
+
+def _has_stamp(d):
+    return d['has_stamp'] 
+
+def _get_measurement_ids(survey_id, detections):
+
+    measurement_ids = []
+
+    if survey_id == 'ztf':
+        for d in detections:
+            if _has_stamp(d):
+                measurement_ids.append(d['candid'])
+    
+    return measurement_ids
+
+def _get_dates(detections):
+
+    dates = []
+    for d in detections:
+        if _has_stamp(d):
+            dates.append(str(Time(d['mjd'], format='mjd').isot))
+
+    return dates
+
+
+#temporal router to develop card
+@router.get('/temp/stamp')
+def temp_stamp(
     oid: str,
-    measurement_id: str,
-    stamp_type: str,
-    file_format: str,
     survey_id: str,
+    request: Request,
 ):
-    handler = handler_selector(survey_id)()
+    total_links = []
 
-    _, file_buffer, mime = handler.get_stamp(
-        oid, measurement_id, stamp_type, file_format
-    )
+    alerce_stamps = AlerceStampsMultisurvey()
+    alerce_search = AlerceSearch()
 
-    return Response(content=file_buffer.getvalue(), media_type=mime)
+    detections = alerce_search.query_detections(oid)
 
+    measurement_ids = _get_measurement_ids(survey_id, detections)
+    dates = _get_dates(detections)
 
-@router.get("/avro")
-async def stamp(
-    request: Request, oid: str, measurement_id: str, survey_id: str
-):
-    handler = handler_selector(survey_id)()
+    image_types = ['science', 'template', 'difference']
 
-    avro_json = handler.get_avro(oid, measurement_id)
+    for i in  range(len(measurement_ids)):
+        stamps = alerce_stamps._get_pgn_stamps(oid = oid, measurement_id = measurement_ids[i], survey_id=survey_id)
+        stamps_dict = dict(zip(image_types, stamps))
+        stamps_with_date = {dates[i]: stamps_dict}
 
-    return avro_json
+        total_links.append(stamps_with_date)
+        
+    return templates.TemplateResponse(
+      name='stamps_card.html.jinja',
+      context={'request': request,
+               'stamps': total_links,
+               'dates': dates,
+               'measurement_ids': measurement_ids
+               },
+  )
