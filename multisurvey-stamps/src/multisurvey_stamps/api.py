e5dc9e018494ebe5f7cfb0c87f2b29c28a248c28
Claudio Mansilla <c.mansillabrito@gmail.com>
2025-09-17 17:51:19 -0300

    added static files

--- multisurvey-stamps/src/multisurvey_stamps/api.py
+++ multisurvey-stamps/src/multisurvey_stamps/api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from .routes import rest
 
 app = FastAPI(openapi_url="/stamps/openapi.json")
@@ -13,6 +14,10 @@ app.add_middleware(
 )
 
 app.include_router(rest.router)
+
+# Mount static files
+app.mount("/static", StaticFiles(directory="src/multisurvey_stamps/static"), name="static")
+app.mount("/htmx", StaticFiles(directory="src/multisurvey_stamps/static/htmx"), name="htmx")
 
 
 @app.get("/openapi.json")
