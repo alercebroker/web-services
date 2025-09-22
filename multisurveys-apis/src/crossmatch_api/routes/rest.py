3401ad571e6605837f9116dccdf7e137d45500a7
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2025-09-22 16:42:27 -0300

    chore: applied linter and format fixes

--- multisurveys-apis/src/crossmatch_api/routes/rest.py
+++ multisurveys-apis/src/crossmatch_api/routes/rest.py
@@ -1,15 +1,10 @@
-from fastapi import APIRouter, HTTPException, Request
-from fastapi.responses import HTMLResponse
+from fastapi import APIRouter
 from fastapi.templating import Jinja2Templates
-
-from core.exceptions import ObjectNotFound
-from ..get_crossmatch_data import get_alerce_data
 
 
 router = APIRouter()
-templates = Jinja2Templates(
-    directory="src/crossmatch_api/templates", autoescape=True, auto_reload=True
-)
+templates = Jinja2Templates(directory="src/crossmatch_api/templates", autoescape=True, auto_reload=True)
+
 
 @router.get("/")
 def root():
