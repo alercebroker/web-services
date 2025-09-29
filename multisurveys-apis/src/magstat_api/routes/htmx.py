ef4ab9f7251adc7c55cdf6c79d9d9b11d4b87794
Claudio Mansilla <c.mansillabrito@gmail.com>
2025-09-29 10:58:16 -0300

    added parser call

--- multisurveys-apis/src/magstat_api/routes/htmx.py
+++ multisurveys-apis/src/magstat_api/routes/htmx.py
@@ -5,8 +5,10 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from core.exceptions import ObjectNotFound
-from core.repository.queries.magstats import get_magstats_by_oid
+# from core.repository.queries.magstats import get_magstats_by_oid
+from ..services.magstats import get_magstats
 from core.repository.dummy_data import magstats_dummy
+from .temporal_utils import mag_parser
 
 
 router = APIRouter()
@@ -20,34 +22,39 @@ templates.env.globals["API_URL"] = os.getenv(
 
 @router.get("/htmx/mag", response_class=HTMLResponse)
 async def object_mag_app(request: Request, oid: str):
-    # try:
-    #     mag_stats = get_mag_stats(
-    #         oid, session_factory=request.app.state.psql_session
-    #     )
-    # except ObjectNotFound:
-    #     raise HTTPException(status_code=404, detail="Object not found")
 
-    # mag_stats_dict = {}
-    # for i, mag_stat in enumerate(mag_stats):
-    #     mag_stats_dict[f"band_{i+1}"] = {
-    #         "stellar": mag_stat.stellar,
-    #         "corrected": mag_stat.corrected,
-    #         "ndet": mag_stat.ndet,
-    #         "ndubious": mag_stat.ndubious,
-    #         "magmean": mag_stat.magmean,
-    #         "magmedian": mag_stat.magmedian,
-    #         "magmax": mag_stat.magmax,
-    #         "magmin": mag_stat.magmin,
-    #         "magsigma": mag_stat.magsigma,
-    #         "maglast": mag_stat.maglast,
-    #         "magfirst": mag_stat.magfirst,
-    #         "firstmjd": mag_stat.firstmjd,
-    #         "lastmjd": mag_stat.lastmjd,
-    #         "step_id_corr": mag_stat.step_id_corr,
-    #         "fid": mag_stat.fid,
-    #     }
 
-    mag_stats_dict = magstats_dummy
+
+    try:
+        mag_stats_raw = get_magstats(
+            oid, session_factory=request.app.state.psql_session
+        )
+    except ObjectNotFound:
+        raise HTTPException(status_code=404, detail="Object not found")
+    mag_stats = mag_parser(mag_stats_raw)
+
+    mag_stats_dict = {}
+
+    for mag_stat in mag_stats:
+        mag_stats_dict[f"band_{mag_stat['fid']}"] = {
+            "stellar": mag_stat['stellar'],
+            "corrected": mag_stat['corrected'],
+            "ndet": mag_stat['ndet'],
+            "ndubious": mag_stat['ndubious'],
+            "magmean": mag_stat['magmean'],
+            "magmedian": mag_stat['magmedian'],
+            "magmax": mag_stat['magmax'],
+            "magmin": mag_stat['magmin'],
+            "magsigma": mag_stat['magsigma'],
+            "maglast": mag_stat['maglast'],
+            "magfirst": mag_stat['magfirst'],
+            "firstmjd": mag_stat['firstmjd'],
+            "lastmjd": mag_stat['lastmjd'],
+            "step_id_corr": mag_stat['step_id_corr'],
+            "fid": mag_stat['fid'],
+        }
+
+    # mag_stats_dict = magstats_dummy
 
     return templates.TemplateResponse(
         name="magstatRebuild.html.jinja",
