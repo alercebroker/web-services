95d10c139d4f738451fc7053fa38abc2d07d0507
Claudio Mansilla <c.mansillabrito@gmail.com>
2025-10-01 09:52:32 -0300

    changed some models to optional because there is still a lot of none in db

--- multisurveys-apis/src/magstat_api/models/magstats.py
+++ multisurveys-apis/src/magstat_api/models/magstats.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-
+from typing import Optional
 
 class MagStat(BaseModel):
     oid: int
@@ -7,23 +7,23 @@ class MagStat(BaseModel):
     band: int
     stellar: bool
     corrected: bool
-    ndubious: int
-    dmdt_first: int
-    dm_first: int
-    sigmadm_first: int
-    dt_first: int
-    magmean: float
-    magmedian: float
-    magmax: float
-    magmin: float
-    magsigma: float
-    maglast: int
-    magfirst: int
-    magmean_corr: float
-    magmedian_corr: float
-    magmax_corr: float
-    magmin_corr: float
-    magsigma_corr: float
-    maglast_corr: float
-    magfirst_corr: float
-    saturation_rate: float
+    ndubious: Optional[int] = None
+    dmdt_first: Optional[int] = None
+    dm_first: Optional[int] = None
+    sigmadm_first: Optional[int] = None
+    dt_first: Optional[int] = None
+    magmean: Optional[float] = None
+    magmedian: Optional[float] = None
+    magmax: Optional[float] = None
+    magmin: Optional[float] = None
+    magsigma: Optional[float] = None
+    maglast: Optional[int] = None
+    magfirst: Optional[int] = None
+    magmean_corr: Optional[float] = None
+    magmedian_corr: Optional[float] = None
+    magmax_corr: Optional[float] = None
+    magmin_corr: Optional[float] = None
+    magsigma_corr: Optional[float] = None
+    maglast_corr: Optional[float] = None
+    magfirst_corr: Optional[float] = None
+    saturation_rate: Optional[float] = None
