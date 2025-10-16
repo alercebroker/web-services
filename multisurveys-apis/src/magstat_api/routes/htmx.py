83aa5af8d4dca5f7036a6c98b60a218373d6ce58
Claudio Mansilla <c.mansillabrito@gmail.com>
2025-10-16 12:33:34 -0300

    added warning if survey_id is not provided. added call to lsst parser.

--- multisurveys-apis/src/magstat_api/routes/htmx.py
+++ multisurveys-apis/src/magstat_api/routes/htmx.py
@@ -7,8 +7,8 @@ from fastapi.templating import Jinja2Templates
 from core.exceptions import ObjectNotFound
 # from core.repository.queries.magstats import get_magstats_by_oid
 from ..services.magstats import get_magstats
-from .temporal_utils import mag_parser
-
+from .temporal_utils import mag_parser, parse_lsst_dia_objects_to_dict
+import warnings
 
 router = APIRouter()
 templates = Jinja2Templates(
@@ -20,27 +20,33 @@ templates.env.globals["API_URL"] = os.getenv(
 
 
 @router.get("/htmx/mag", response_class=HTMLResponse)
-async def object_mag_app(request: Request, oid: str):
-
+async def object_mag_app(request: Request, oid: str, survey_id=None):
     bandMapping = {
         1: "g",
         2: "r",
         3: "i",
     };
 
+    if survey_id == None:
+        survey_id = "ztf"
+        warnings.warn(
+            "survey not provided, defaulting to 'ztf'. This will use the legacy ZTF client. This behavior will be deprecated in future versions.",
+            DeprecationWarning,
+        )
 
     try:
         mag_stats_raw = get_magstats(
-            oid, session_factory=request.app.state.psql_session
-        )
+            oid, survey_id, session_factory=request.app.state.psql_session
+        ) 
     except ObjectNotFound:
         raise HTTPException(status_code=404, detail="Object not found")
+    if survey_id == "ztf":
+        mag_stats = mag_parser(mag_stats_raw)
+        for d in mag_stats:
+            del d['fid']
+    elif survey_id == "lsst":
+        mag_stats = parse_lsst_dia_objects_to_dict(mag_stats_raw)
     
-    mag_stats = mag_parser(mag_stats_raw)
-
-    for d in mag_stats:
-        del d['fid']
-
     mag_keys = list(mag_stats[0].keys())
     return templates.TemplateResponse(
         name="magstatRebuild.html.jinja",
