986b0fad3034ffbb4d1947ff136b56115f88973a
Claudio Mansilla <c.mansillabrito@gmail.com>
2025-10-17 10:23:52 -0300

    added diferent class from ztf and lsst, deleted created_date.

--- multisurveys-apis/src/magstat_api/routes/rest.py
+++ multisurveys-apis/src/magstat_api/routes/rest.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Request
 from fastapi import HTTPException
 from ..services.magstats import get_magstats
 from core.repository.dummy_data import magstats_dummy
+from .temporal_utils import mag_parser, parse_lsst_dia_objects_to_dict
+from core.exceptions import ObjectNotFound
 
 router = APIRouter()
 
@@ -15,10 +17,21 @@ async def ping():
 async def magstats(
     request: Request,
     oid: str,
+    survey_id: str
 ):
-    magstats = get_magstats(oid, session_factory=request.app.state.psql_session)
+    try:
+        mag_stats_raw = get_magstats(
+            oid, survey_id, session_factory=request.app.state.psql_session
+        ) 
+    except ObjectNotFound:
+        raise HTTPException(status_code=404, detail="Object not found")
+    
+    if survey_id == "ztf":
+        mag_stats = mag_parser(mag_stats_raw)
+        for d in mag_stats:
+            del d['fid']
+    elif survey_id == "lsst":
+        mag_stats = parse_lsst_dia_objects_to_dict(mag_stats_raw)
+        del mag_stats[0]["created_date"]
 
-    if not magstats:
-        raise HTTPException(status_code=404, detail="Magstats not found for the given OID")
-
-    return magstats
+    return mag_stats
