63c7653e52301cc7314c91a639745a675d9eb0dc
Alex Alvarez <otajla@hotmail.com>
2025-11-13 03:55:34 -0300

    somewhat working need help still

--- multisurveys-apis/src/stamps_api/models/lightcurve.py
+++ multisurveys-apis/src/stamps_api/models/lightcurve.py
@@ -14,3 +14,9 @@ class detection(BaseModel):
 
 class LightcurveModel(BaseModel):
     detections : list[detection] = []
+
+class PostRequestInputModel(BaseModel):
+    oid: str
+    survey_id: str
+    measurement_id: int
+    detections_list: list[dict]
