8cf4c444917499ab2716afbe5de00b67e896162c
Alex Alvarez <otajla@hotmail.com>
2025-12-16 15:39:57 -0300

    added stamps configs to run all

--- multisurveys-apis/scripts/run_api.py
+++ multisurveys-apis/scripts/run_api.py
@@ -166,6 +166,10 @@ def run_service(
     os.environ["PSQL_PORT"] = str(db_config["psql_port"])
     os.environ["SCHEMA"] = db_config["psql_schema"]
     os.environ["USE_ABSOLUTE"] = config_dict.get("use_absolute", "false")
+    os.environ["LSST_BUCKET_REGION"] = config_dict.get("lsst_bucket_region", "")
+    os.environ["LSST_BUCKET_NAME"] = config_dict.get("lsst_bucket_name", "")
+    os.environ["ZTF_BUCKET_REGION"] = config_dict.get("ztf_bucket_region", "")
+    os.environ["ZTF_BUCKET_NAME"] = config_dict.get("ztf_bucket_name", "")
 
     uvicorn.run(
         f"src.{config_dict['source_folder']}.api:app",
