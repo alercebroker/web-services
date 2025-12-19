ff43fd5bade614a728e4ec6e790ac8a128389cf9
Claudio Mansilla <c.mansillabrito@gmail.com>
2025-12-19 17:04:24 -0300

    modified function to put oir and mid on archive name

--- multisurveys-apis/src/stamps_api/static/stamp.js
+++ multisurveys-apis/src/stamps_api/static/stamp.js
@@ -1,31 +1,30 @@
-
-
 let scienceDownload = document.getElementById("scienceDownload")
 let templateDownload = document.getElementById("templateDownload")
 let differenceDownload = document.getElementById("differenceDownload")
 
-scienceDownload.addEventListener("click", () => downloadStamp("science"))
-templateDownload.addEventListener("click", () => downloadStamp("template"))
-differenceDownload.addEventListener("click", () => downloadStamp("difference"))
+scienceDownload.addEventListener("click", () => downloadStamp("science", scienceDownload))
+templateDownload.addEventListener("click", () => downloadStamp("template", templateDownload))
+differenceDownload.addEventListener("click", () => downloadStamp("difference", differenceDownload))
 
-export function downloadStamp(stampType) {
-    const imageIdMap = {
+export function downloadStamp(stampType, buttonElement) {
+  const imageIdMap = {
     "science": "scienceImg",
     "template": "templateImg",
     "difference": "differenceImg"
-    }
+  }
 
-    const imgElement = document.getElementById(imageIdMap[stampType])
-    if (!imgElement) {
+  const imgElement = document.getElementById(imageIdMap[stampType])
+  if (!imgElement) {
     console.error(`Image element not found for stamp type: ${stampType}`)
     return
-    }
+  }
 
-    const dataUrl = imgElement.src
-    const link = document.createElement("a")
-    link.href = dataUrl
-    link.download = `stamp_${stampType}.png`
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
+  const oidMid = buttonElement.getAttribute("content")
+  const dataUrl = imgElement.src
+  const link = document.createElement("a")
+  link.href = dataUrl
+  link.download = `${oidMid}_${stampType}.png`
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
 }
