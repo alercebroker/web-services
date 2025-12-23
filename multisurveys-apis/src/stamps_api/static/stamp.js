db11764f8fa7057ade8b82fe5a4bab141c90de96
Claudio Mansilla <c.mansillabrito@gmail.com>
2025-12-23 15:28:21 -0300

    added arrow from mjd logic. download logic, zoom image and zoomm button logic

--- multisurveys-apis/src/stamps_api/static/stamp.js
+++ multisurveys-apis/src/stamps_api/static/stamp.js
@@ -1,10 +1,4 @@
-let scienceDownload = document.getElementById("scienceDownload")
-let templateDownload = document.getElementById("templateDownload")
-let differenceDownload = document.getElementById("differenceDownload")
 
-scienceDownload.addEventListener("click", () => downloadStamp("science", scienceDownload))
-templateDownload.addEventListener("click", () => downloadStamp("template", templateDownload))
-differenceDownload.addEventListener("click", () => downloadStamp("difference", differenceDownload))
 
 export function downloadStamp(stampType, buttonElement) {
   const imageIdMap = {
@@ -28,3 +22,102 @@ export function downloadStamp(stampType, buttonElement) {
   link.click()
   document.body.removeChild(link)
 }
+
+export function init() {
+
+  // Download button elements
+  let scienceDownload = document.getElementById("scienceDownload")
+  let templateDownload = document.getElementById("templateDownload")
+  let differenceDownload = document.getElementById("differenceDownload")
+
+  // MJD dropdown arrow elements
+  let mjdArrowWrapper = document.getElementById("mid_arrow");
+  let selectElement = document.getElementById("measurement_id");
+
+  // Zoom image elements
+  let imageZoom = document.getElementById("imageZoom");
+
+  // Zoom button elements
+  let zoomStamps = document.getElementById("zoomStamp")
+
+  // Download button eventListeners 
+  scienceDownload.addEventListener("click", () => downloadStamp("science", scienceDownload))
+  templateDownload.addEventListener("click", () => downloadStamp("template", templateDownload))
+  differenceDownload.addEventListener("click", () => downloadStamp("difference", differenceDownload))
+
+  // MJD dropdown arrow eventListeners
+  selectElement.addEventListener("mousedown", () => {
+    set_arrow_direction(mjdArrowWrapper);
+  });
+  selectElement.addEventListener("mouseup", () => {
+    set_arrow_direction(mjdArrowWrapper);
+  });
+
+  // Zoom image eventListeners
+  imageZoom.addEventListener('mousemove', (event) => {
+    imageZoom.style.setProperty('--display', 'block');
+
+    let rect = imageZoom.getBoundingClientRect();
+
+    let pointer = {
+      x: ((event.clientX - rect.left) * 100) / rect.width,
+      y: ((event.clientY - rect.top) * 100) / rect.height
+    }
+
+    imageZoom.style.setProperty('--zoom-x', pointer.x + '%');
+    imageZoom.style.setProperty('--zoom-y', pointer.y + '%');
+
+  })
+  imageZoom.addEventListener('mouseleave', () => {
+    imageZoom.style.setProperty('--display', 'none');
+  })
+
+  // Zoom button eventListeners
+  zoomStamps.addEventListener('click', () => {
+    let currentVal = getComputedStyle(imageZoom).getPropertyValue('--z-index').trim();
+
+    if (parseInt(currentVal) > 0) {
+      imageZoom.style.setProperty('--z-index', -1)
+    } else {
+      imageZoom.style.setProperty('--z-index', 1)
+    }
+  })
+}
+
+function set_arrow_direction(container) {
+  let svg = container.querySelector('svg[name="arrow_icon"]');
+  if (!svg) return;
+
+  let path = svg.querySelector('path');
+
+  let currentDirection = svg.dataset.direction;
+
+  if (currentDirection === "down") {
+    svg.dataset.direction = 'up';
+    path.setAttribute('d', "M480-528 296-344l-56-56 240-240 240 240-56 56-184-184Z");
+  } else {
+    svg.dataset.direction = 'down';
+    path.setAttribute('d', "M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z");
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
