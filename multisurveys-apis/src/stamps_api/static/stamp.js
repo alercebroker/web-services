6400a5f2ff3b4585d696ab21796db3f5547f112e
Claudio Mansilla <c.mansillabrito@gmail.com>
2025-12-31 12:56:29 -0300

    fixed border case. now when the zoom is on and put the crosshair, the zoom turns off

--- multisurveys-apis/src/stamps_api/static/stamp.js
+++ multisurveys-apis/src/stamps_api/static/stamp.js
@@ -68,11 +68,15 @@ export function init() {
   // Zoom button eventListeners
   zoomStamps.addEventListener('click', () => {
     toggleZoom()
+    if (!stampsContainer.classList.contains('hide-crosshairs')) {
+      stampsContainer.classList.toggle('hide-crosshairs')
+    }
   })
 
   // Crosshair buttom eventListeners
   crossHair.addEventListener('click', () => {
     stampsContainer.classList.toggle('hide-crosshairs')
+    toggleZoom(true)
   })
 }
 
@@ -127,23 +131,19 @@ function makeZoomWork(imagesZoom) {
   }
 }
 
-function toggleZoom() {
+function toggleZoom(crossFlag = false) {
 
   let currentVal = getComputedStyle(scienceZoom).getPropertyValue('--z-index').trim();
 
-  if (parseInt(currentVal) > 0) {
+  if (parseInt(currentVal) > 0 || crossFlag) {
     scienceZoom.style.setProperty('--z-index', -1)
     templateZoom.style.setProperty('--z-index', -1)
     differenceZoom.style.setProperty('--z-index', -1)
-
+    crossFlag = false
   } else {
     scienceZoom.style.setProperty('--z-index', 1)
     templateZoom.style.setProperty('--z-index', 1)
     differenceZoom.style.setProperty('--z-index', 1)
-
-  }
-  if (!stampsContainer.classList.contains('hide-crosshairs')) {
-    stampsContainer.classList.toggle('hide-crosshairs')
   }
 }
 
