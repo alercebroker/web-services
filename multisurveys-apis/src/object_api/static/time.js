57e89ad1edd83ca17e2fa6ab4960eaaa87c1d52f
Claudio Mansilla <c.mansillabrito@gmail.com>
2026-01-07 10:03:07 -0300

    changed broken link of skypack for moment library, now using jsdelivr

--- multisurveys-apis/src/object_api/static/time.js
+++ multisurveys-apis/src/object_api/static/time.js
@@ -1,32 +1,32 @@
-import moment from 'https://cdn.skypack.dev/moment'
+import moment from 'https://cdn.jsdelivr.net/npm/moment/+esm'
 
 export function getUTCDate(date) {
-    if (!date) return null
-    const dateStr = date.toUTCString()
-    return moment.utc(dateStr).toDate()
+  if (!date) return null
+  const dateStr = date.toUTCString()
+  return moment.utc(dateStr).toDate()
 }
-  
+
 export function extractDate(datetime) {
-    if (!datetime) return null
-    return moment.utc(datetime).format('YYYY-MM-DD')
+  if (!datetime) return null
+  return moment.utc(datetime).format('YYYY-MM-DD')
 }
 
 export function extractTime(datetime) {
-    if (!datetime) return null
-    return moment.utc(datetime).format('HH:mm')
+  if (!datetime) return null
+  return moment.utc(datetime).format('HH:mm')
 }
 
 export function convertToDate(date, time) {
-    let strDate = date + '-' + time
-    if (date && !time) {
-        strDate = date + '-' + '00:00'
-    }
-    if (!date) {
-        return null
-    }
-    return moment.utc(strDate, 'YYYY-MM-DD-HH:mm').toDate()
+  let strDate = date + '-' + time
+  if (date && !time) {
+    strDate = date + '-' + '00:00'
+  }
+  if (!date) {
+    return null
+  }
+  return moment.utc(strDate, 'YYYY-MM-DD-HH:mm').toDate()
 }
 
 export function formatDate(date) {
-    return moment.utc(date).format()
+  return moment.utc(date).format()
 }
