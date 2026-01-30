09f70a7c4efeacf676ac10b44bc8319e720e120b
Demurest <eric.apolonio@hotmail.com>
2026-01-30 16:54:44 -0300

    add measurement id tag in hover

--- multisurveys-apis/src/lightcurve_api/services/lightcurve_plot_service/chart_point.py
+++ multisurveys-apis/src/lightcurve_api/services/lightcurve_plot_service/chart_point.py
@@ -6,16 +6,18 @@ class ChartPoint:
     band: str
     x: float
     y: float
+    measurement_id: str
 
-    def __init__(self, survey: str, band: str, x: float, y: float, error: float):
+    def __init__(self, survey: str, band: str, x: float, y: float, error: float, measurement_id: str = None):
         self.survey = survey
         self.band = band
         self.x = x
         self.y = y
         self.error = error
+        self.measurement_id = measurement_id
 
     def point(self) -> List[float]:
-        return [self.x, self.y]
+        return [self.x, self.y, self.measurement_id]
 
     def error_bar(self, limit=1) -> List[float]:
         err = self.error if self.error <= limit else limit
