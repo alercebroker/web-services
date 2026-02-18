7e353f8976f979c2333c558c3bb94f4d733f220a
Claudio Mansilla <c.mansillabrito@gmail.com>
2026-02-18 20:15:33 -0300

    added absolute and redshift as parameters into functions

--- multisurveys-apis/src/lightcurve_api/models/lightcurve_item.py
+++ multisurveys-apis/src/lightcurve_api/models/lightcurve_item.py
@@ -9,44 +9,52 @@ class BaseDetection(BaseModel):
     ra: float
     dec: float
 
-    def magnitude2flux(self, total: bool, absolute: bool) -> float:
+    def magnitude2flux(self, total: bool, absolute: bool, redshift: float) -> float:
         """Convert magnitude to flux.
 
         Args:
             total: Whether to use corrected magnitude (True) or uncorrected (False)
+            absolute: Whether to use absolute magnitude (True) or apparent magnitude (False)
+            redshift: Redshift value for absolute magnitude calculation
 
         Returns:
             Calculated flux value
         """
         raise NotImplementedError
 
-    def magnitude2flux_err(self, total: bool, absolute: bool) -> float:
+    def magnitude2flux_err(self, total: bool, absolute: bool, redshift: float) -> float:
         """Calculate flux error from magnitude error.
 
         Args:
             total: Whether to use corrected error (True) or uncorrected (False)
+            absolute: Whether to use absolute magnitude (True) or apparent magnitude (False)
+            redshift: Redshift value for absolute magnitude calculation
 
         Returns:
             Calculated flux error
         """
         raise NotImplementedError
 
-    def flux2magnitude(self, total: bool, absolute: bool) -> float:
+    def flux2magnitude(self, total: bool, absolute: bool, redshift: float) -> float:
         """Convert flux to magnitude.
 
         Args:
             total: Whether to use corrected flux (True) or uncorrected (False)
+            absolute: Whether to use absolute magnitude (True) or apparent magnitude (False)
+            redshift: Redshift value for absolute magnitude calculation
 
         Returns:
             Calculated magnitude value
         """
         raise NotImplementedError
 
-    def flux2magnitude_err(self, total: bool, absolute: bool) -> float:
+    def flux2magnitude_err(self, total: bool, absolute: bool, redshift: float) -> float:
         """Get magnitude error from flux error.
 
         Args:
             total: Whether to use corrected error (True) or uncorrected (False)
+            absolute: Whether to use absolute magnitude (True) or apparent magnitude (False)
+            redshift: Redshift value for absolute magnitude calculation
 
         Returns:
             Magnitude error value
@@ -84,44 +92,52 @@ class BaseForcedPhotometry(BaseModel):
     mjd: float
     survey_id: str
 
-    def magnitude2flux(self, total: bool) -> float:
+    def magnitude2flux(self, total: bool, absolute: bool, redshift: float) -> float:
         """Convert magnitude to flux.
 
         Args:
             total: Whether to use corrected magnitude (True) or uncorrected (False)
+            absolute: Whether to use absolute magnitude (True) or apparent magnitude (False)
+            redshift: Redshift value for absolute magnitude calculation
 
         Returns:
             Calculated flux value
         """
         raise NotImplementedError
 
-    def magnitude2flux_err(self, total: bool) -> float:
+    def magnitude2flux_err(self, total: bool, absolute: bool, redshift: float) -> float:
         """Calculate flux error from magnitude error.
 
         Args:
             total: Whether to use corrected error (True) or uncorrected (False)
+            absolute: Whether to use absolute magnitude (True) or apparent magnitude (False)
+            redshift: Redshift value for absolute magnitude calculation
 
         Returns:
             Calculated flux error
         """
         raise NotImplementedError
 
-    def flux2magnitude(self, total: bool) -> float:
+    def flux2magnitude(self, total: bool, absolute: bool, redshift: float) -> float:
         """Convert flux to magnitude.
 
         Args:
             total: Whether to use corrected flux (True) or uncorrected (False)
+            absolute: Whether to use absolute magnitude (True) or apparent magnitude (False)
+            redshift: Redshift value for absolute magnitude calculation
 
         Returns:
             Calculated magnitude value
         """
         raise NotImplementedError
 
-    def flux2magnitude_err(self, total: bool) -> float:
+    def flux2magnitude_err(self, total: bool, absolute: bool, redshift: float) -> float:
         """Get magnitude error from flux error.
 
         Args:
             total: Whether to use corrected error (True) or uncorrected (False)
+            absolute: Whether to use absolute magnitude (True) or apparent magnitude (False)
+            redshift: Redshift value for absolute magnitude calculation
 
         Returns:
             Magnitude error value
@@ -135,3 +151,7 @@ class BaseForcedPhotometry(BaseModel):
     def phase(self, period: float) -> float:
         """Calculate the phase for the given period"""
         return (self.mjd % period) / period
+
+    def flux_sign(self, total: bool, absolute: bool, redshift: float) -> str:
+        """Get the flux sign."""
+        raise NotImplementedError
