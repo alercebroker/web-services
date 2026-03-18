83806e295bc7a07f24154d4e4934721c5a50f777
Ignacio Reyes <ireyesj93@gmail.com>
2026-03-17 23:00:06 -0300

    unnecessary gzip in lightcurve api, julian external dependency removed, unnecessary delay in lc widget for change trigger

---
+++ multisurveys-apis/src/static/julian.js
@@ -0,0 +1,28 @@
+// Library from https://github.com/stevebest/julian licensed under MIT License
+
+var DAY = 86400000;
+var HALF_DAY = DAY / 2;
+var UNIX_EPOCH_JULIAN_DATE = 2440587.5;
+var UNIX_EPOCH_JULIAN_DAY = 2440587;
+
+function convert(date) {
+  return (toJulianDay(date) + (toMillisecondsInJulianDay(date) / DAY)).toFixed(6);
+};
+
+function convertToDate(julian) {
+  return new Date((Number(julian) - UNIX_EPOCH_JULIAN_DATE) * DAY);
+};
+
+function toJulianDay(date) {
+  return ~~((+date + HALF_DAY) / DAY) + UNIX_EPOCH_JULIAN_DAY;
+};
+
+function toMillisecondsInJulianDay(date) {
+  return (+date + HALF_DAY) % DAY;
+};
+
+function fromJulianDayAndMilliseconds(day, ms) {
+  return (day - UNIX_EPOCH_JULIAN_DATE) * DAY + ms;
+};
+
+export default { convert, toDate: convertToDate, toJulianDay, toMillisecondsInJulianDay, fromJulianDayAndMilliseconds };
