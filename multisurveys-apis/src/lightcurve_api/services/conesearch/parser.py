0da461f7fd469d449f26b179789325ef604e66cd
Alex Alvarez <otajla@hotmail.com>
2026-03-18 16:06:41 -0300

    clean up a little the cache interface

--- multisurveys-apis/src/lightcurve_api/services/conesearch/parser.py
+++ multisurveys-apis/src/lightcurve_api/services/conesearch/parser.py
@@ -1,9 +1,15 @@
+import json
 from typing import List, cast
 from db_plugins.db.sql.models import Object
 
 from lightcurve_api.models.object import ApiObject
 from core.idmapper.idmapper import decode_masterid
 from numpy import int64
+
+from ...models.lightcurve_item import BaseDetection, BaseForcedPhotometry, BaseNonDetection
+from ...models.detections import LsstDetection, ztfDetection
+from ...models.force_photometry import LsstForcedPhotometry, ZtfForcedPhotometry
+from ...models.non_detections import ZtfNonDetections
 
 
 def survey_id_map(sid: int) -> str:
@@ -29,3 +35,42 @@ def parse_api_object(sql_object: Object) -> ApiObject:
 
 def parsesapi_objects(sql_objects: List[Object]) -> List[ApiObject]:
     return [parse_api_object(sql_object) for sql_object in sql_objects]
+
+
+def parse_lightcurve_model_to_jsonstring(
+        list_instances: list[BaseDetection] | list[BaseForcedPhotometry] | list[BaseNonDetection]
+    ) -> str:
+    """Parses a lightcurve model (e.g. list of detections) to a JSON string for caching."""
+
+    list_of_dicts = [instance.model_dump() for instance in list_instances]
+    return json.dumps(list_of_dicts)
+
+def parse_jsonstring_to_lightcurve_model(
+        json_string: str,
+        model_class: BaseDetection | BaseForcedPhotometry | BaseNonDetection,
+    ) -> list[BaseDetection] | list[BaseForcedPhotometry] | list[BaseNonDetection] :
+    """Parses a JSON string back into a lightcurve model."""
+
+    list_of_dicts = json.loads(json_string)
+    return [model_class(**d) for d in list_of_dicts]  
+
+class DetectionCacheUtil():
+    key = "detections"
+    models = {
+        "ztf": ztfDetection,
+        "lsst": LsstDetection,
+    }
+
+class ForcedPhotometryCacheUtil():
+    key = "forced_photometry"
+    models = {
+        "ztf": ZtfForcedPhotometry,
+        "lsst": LsstForcedPhotometry,
+    }
+
+class NonDetectionsCacheUtil():
+    key = "non_detections"
+    models = {
+        "ztf": ZtfNonDetections,
+        "lsst": BaseNonDetection,  # we currently don't have a separate non-detections model for lsst, so we use the base model
+    }
--- multisurveys-apis/src/lightcurve_api/services/lightcurve_service.py
+++ multisurveys-apis/src/lightcurve_api/services/lightcurve_service.py
@@ -5,6 +5,7 @@ from typing import Callable, ContextManager, List, Tuple, cast
 from sqlalchemy.orm import Session
 from toolz import pipe
 
+from core.config.cache import CacheEntity
 from core.idmapper import idmapper
 from core.repository.queries import detections as detection_repository
 from core.repository.queries import non_detections as non_detections_repository
@@ -24,72 +25,34 @@ from .parsers import (
 )
 from ..models.detections import LsstDetection
 from ..models.force_photometry import LsstForcedPhotometry
+from valkey import Valkey
+from ..services.conesearch.parser import parse_lightcurve_model_to_jsonstring, parse_jsonstring_to_lightcurve_model, DetectionCacheUtil, ForcedPhotometryCacheUtil, NonDetectionsCacheUtil
 
-import valkey
-
-VALKEY_HOST = "localhost" #os.getenv("VALKEY_HOST")
-VALKEY_PORT = 6379 #os.getenv("VALKEY_PORT")
-
-def get_glide_client():
-    # TODO: we should not be creating a new client for every request, 
-    # but this is a temporary solution until we have a better way to manage the client lifecycle
-    print("creating cache client")
-    client = valkey.Valkey(host=VALKEY_HOST, port=VALKEY_PORT)
-    response = client.ping()
-    print(f"Successfully connected to Glide Valkey at {VALKEY_HOST}:{VALKEY_PORT} - Ping response: {response}")
-    return client
-
-
-def get_cache_value(key: str):
-    client = get_glide_client()
-    value = client.get(key)
-    print(f"\t --- \t -- Retrieved cache value for key: {key} \n value: {value}")
-    return value
-
-def set_cache_value(key: str, value):
-    client = get_glide_client()
-    client.set(key, str(value))
-
-def cache_query_wrapper(cache_key_prefix):
+def cache_query_wrapper(
+        cache_util: DetectionCacheUtil | ForcedPhotometryCacheUtil | NonDetectionsCacheUtil,
+    ):
     def decorator(func):
         def wrapper(*args, **kwargs):
-            print(f"HERE ARE THE ARGS:\n {args} \n {kwargs}")
+            cache_client: Valkey = CacheEntity()
             oid = args[0]
             survey_id = args[1]
-            cache_key = f"{cache_key_prefix}:{oid}:{survey_id}"
-            print(f"Checking cache for key: {cache_key}")
-            cached_result = get_cache_value(cache_key)
-            #cached_result = None # disable cache for now
+            cache_key = f"{cache_util.key}:{survey_id}:{oid}"
+            cached_result = cache_client.get(cache_key)
+
             if cached_result is not None:
-                print(f"Cache hit for key: {cache_key} \n with result: {cached_result}")
-                cache_loaded = json.loads(cached_result)
-                print(f"\n\n ------ \n -- \nCache loaded\n loaded value: {cache_loaded}")
-                if cache_key_prefix == "detections":
-                    result_model = [LsstDetection(**r) for r in cache_loaded]
-                else:
-                    result_model = [LsstForcedPhotometry(**r) for r in cache_loaded]
+                result_model = parse_jsonstring_to_lightcurve_model(cached_result, cache_util.models[survey_id])
                 return result_model
             
-            print("Cache miss, executing query...")
             result = func(*args, **kwargs)
-            print(f"Query result: {result}")
-            result_parsed = [r.model_dump() for r in result]
-            result_parsed_json = json.dumps(result_parsed)
-            print(f"\n---\n parsed result: {result_parsed_json}\n---\n")
-            # if cache_key_prefix == "detections":
-            #     result_model = [LsstDetection(**json.loads(r)) for r in result_parsed]
-            # else:
-            #     result_model = [LsstForcedPhotometry(**json.loads(r)) for r in result_parsed]
-            # print(f"\n--    --\n result model: {result_model}\n--    --\n")
-            print(f"Setting cache for key: {cache_key} \n with result: {result_parsed_json}")
-            set_cache_value(cache_key, result_parsed_json)
+            result_parsed_json = parse_lightcurve_model_to_jsonstring(result)
+            cache_client.set(cache_key, result_parsed_json)
             return result
         
         return wrapper
     
     return decorator
 
-@cache_query_wrapper("detections")
+@cache_query_wrapper(DetectionCacheUtil)
 def get_detections(
     oid: str,
     survey_id: str,
@@ -101,13 +64,12 @@ def get_detections(
 
     return result
 
-@cache_query_wrapper("detections")
+@cache_query_wrapper(DetectionCacheUtil)
 def get_detections_by_list(
     oids: List[str],
     survey_id: str,
     session_factory: Callable[..., ContextManager[Session]],
 ) -> List[BaseDetection]:
-    print(f"\t -- \tGetting detections for OIDs: {oids} and survey_id: {survey_id}")
     result = cast(
         List[BaseDetection],
         pipe(
@@ -117,7 +79,6 @@ def get_detections_by_list(
             parse_sql_detection,
         ),
     )
-    print(f"\t-- \tRetrieved {len(result)} detections")
     return result
 
 
@@ -128,7 +89,7 @@ def convert_oid_list_to_int(
 
     return [idmapper.catalog_oid_to_masterid(survey_id, oid).item() for oid in oid_list], survey_id
 
-@cache_query_wrapper("non_detections")
+@cache_query_wrapper(NonDetectionsCacheUtil)
 def get_non_detections(
     oid: str,
     survey_id: str,
@@ -144,7 +105,7 @@ def get_non_detections(
 
     return result_parsed
 
-@cache_query_wrapper("non_detections")
+@cache_query_wrapper(NonDetectionsCacheUtil)
 def get_non_detections_by_list(
     oids: List[str],
     survey_id: str,
@@ -160,7 +121,7 @@ def get_non_detections_by_list(
         ),
     )
 
-@cache_query_wrapper("forced_photometry")
+@cache_query_wrapper(ForcedPhotometryCacheUtil)
 def get_forced_photometry(
     oid: str,
     survey_id: str,
@@ -176,7 +137,7 @@ def get_forced_photometry(
 
     return result_parsed
 
-@cache_query_wrapper("forced_photometry")
+@cache_query_wrapper(ForcedPhotometryCacheUtil)
 def get_forced_photometry_by_list(
     oids: List[str],
     survey_id: str,
