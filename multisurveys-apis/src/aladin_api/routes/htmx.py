6740263f977c4da617ea0a4a44580b78fce500fe
demurest <eric.apolonio@hotmail.com>
2026-09-10 11:55:15 -0300

    update(aladin): add encode ids functionality

--- multisurveys-apis/src/aladin_api/routes/htmx.py
+++ multisurveys-apis/src/aladin_api/routes/htmx.py
@@ -6,20 +6,24 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from ..services.aladin_services import get_object_by_id
 from ..services.aladin_parser import loads_objects_list
+from core.idmapper.idmapper import encode_ids
+
 
 router = APIRouter(prefix="/htmx")
 templates = Jinja2Templates(directory="src/aladin_api/templates", autoescape=True, auto_reload=True)
 templates.env.globals["API_URL"] = os.getenv("API_URL", "http://localhost:8006")
 
-
+import pprint
 @router.post("/aladin", response_class=HTMLResponse)
-async def object_probability_app(request: Request, oid: str, objects_arr: Optional[str] = Form(None)):
+async def object_probability_app(request: Request, oid: str, survey: str, objects_arr: Optional[str] = Form(None)):
     try:
         session_ms = request.app.state.psql_session
 
+        master_oid = encode_ids(survey, [oid])
+
         objects_list = loads_objects_list(objects_arr)
-        survey = ""
-        selected_object = get_object_by_id(session_ms, oid, survey)
+
+        selected_object = get_object_by_id(session_ms, int(master_oid[0]), "")
     except HTTPException:
         traceback.print_exc()
         raise HTTPException(status_code=500, detail="An error occurred")
@@ -30,25 +34,25 @@ async def object_probability_app(request: Request, oid: str, objects_arr: Option
     )
 
 
-@router.get("/aladin", response_class=HTMLResponse)
-async def object_probability_app_get(
-    request: Request,
-    oid: Optional[str] = None,
-    objects_arr: Optional[List[str]] = Query(None),
-):
-    try:
-        session_ms = request.app.state.psql_session
+# @router.get("/aladin", response_class=HTMLResponse)
+# async def object_probability_app_get(
+#     request: Request,
+#     oid: Optional[str] = None,
+#     objects_arr: Optional[List[str]] = Query(None),
+# ):
+#     try:
+#         session_ms = request.app.state.psql_session
 
-        objects_list = loads_objects_list(objects_arr)
-        survey = ""
-        selected_object = None
-        if oid is not None:
-            selected_object = get_object_by_id(session_ms, oid, survey)
-    except HTTPException:
-        traceback.print_exc()
-        raise HTTPException(status_code=500, detail="An error occurred")
+#         objects_list = loads_objects_list(objects_arr)
+#         survey = ""
+#         selected_object = None
+#         if oid is not None:
+#             selected_object = get_object_by_id(session_ms, oid, survey)
+#     except HTTPException:
+#         traceback.print_exc()
+#         raise HTTPException(status_code=500, detail="An error occurred")
 
-    return templates.TemplateResponse(
-        name="layout.html.jinja",
-        context={"request": request, "objects": objects_list, "selected_object": selected_object},
-    )
+#     return templates.TemplateResponse(
+#         name="layout.html.jinja",
+#         context={"request": request, "objects": objects_list, "selected_object": selected_object},
+#     )
