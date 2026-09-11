e3fc63627bff44ddd5cac45e76e32ab8eed69e15
demurest <eric.apolonio@hotmail.com>
2026-09-11 09:34:24 -0300

    refactor(xmatch): clean code of htmx side

---
+++ multisurveys-apis/src/crossmatch_api/services/crossmatch_service.py
@@ -0,0 +1,52 @@
+from sqlalchemy.orm import Session
+from crossmatch_api.get_crossmatch_data import get_alerce_data
+from core.idmapper.idmapper import encode_ids
+from core.repository.queries.objects import (
+    query_object_by_id,
+)
+from ..models.object import Object
+from .parsers import parse_object
+
+
+def get_cross_data(oid:str, survey_id:str, session: Session):
+
+    object = get_object_data(oid, survey_id, session)
+    cross = get_alerce_data(object.meanra, object.meandec, 20)
+    cross_keys = get_keys(cross)
+
+
+    return cross, cross_keys
+
+
+
+def get_object_data(oid: str, survey: str, session: Session) -> Object:
+    master_oid = encode_ids(survey, [oid])[0]
+
+    object_ztf, object = query_object_by_id(session, int(master_oid), survey)
+    object = parse_object(object)
+
+    return object
+
+
+def get_keys(data) -> list:
+    """
+    Extract the top-level key from each dictionary in a list of single-key dictionaries.
+
+    Expects `data` to be a list where each element is a dict containing exactly
+    one key-value pair, e.g.:
+
+        [{'name1': {...}}, {'name2': {...}}, {'name3': {...}}]
+
+    Returns a list of those keys in order, e.g.:
+
+        ['name1', 'name2', 'name3']
+
+    Uses next(iter(d)) on each dict to grab its single key without building
+    a full list of keys first.
+    """
+
+    response = []
+    for i in range(len(data)):
+        response.append(next(iter(data[i].keys())))
+
+    return response
